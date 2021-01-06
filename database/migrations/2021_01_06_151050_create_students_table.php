898062ee4e00c7ac879fda42e52ac5c3705986ac
babalolajnr <babalolajnr@gmail.com>
2021-01-06 16:37:49 +0100

    created Student Model, Migration ...

---
+++ database/migrations/2021_01_06_151050_create_students_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('sex');
+            $table->string('lg');
+            $table->string('state');
+            $table->string('country');
+            $table->date('date_of_birth');
+            $table->string('status');
+            $table->foreignId('guardian_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
