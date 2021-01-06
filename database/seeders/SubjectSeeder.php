1da9233b337a0c122c7efa888c76ab03d74c8d78
babalolajnr <babalolajnr@gmail.com>
2021-01-06 16:54:40 +0100

    created subject model ...

---
+++ database/seeders/SubjectSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class SubjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
