eb59fdfbc39ea1d05ebc757a3744095aa4ae0899
babalolajnr <babalolajnr@gmail.com>
2021-01-06 16:04:57 +0100

    added first_name, last_name and user_type fields

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,9 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
+            'first_name' => $this->faker->name,
+            'last_name' => $this->faker->name,
+            'user_type' => $this->faker->randomElement(['admin', 'master']),
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
