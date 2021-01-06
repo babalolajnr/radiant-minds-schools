33a6261519435198d1ac0922ee56c8912625fa75
babalolajnr <babalolajnr@gmail.com>
2021-01-06 17:24:51 +0100

    added name column

--- database/migrations/2021_01_06_155123_create_classrooms_table.php
+++ database/migrations/2021_01_06_155123_create_classrooms_table.php
@@ -15,6 +15,7 @@ class CreateClassroomsTable extends Migration
     {
         Schema::create('classrooms', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
