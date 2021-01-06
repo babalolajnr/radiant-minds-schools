95df68f5762e5981da16697eb67b90932b1f7cd6
babalolajnr <babalolajnr@gmail.com>
2021-01-06 17:39:50 +0100

    added classrooms relationship

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -10,4 +10,9 @@ class Subject extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public function classrooms()
+    {
+        $this->belongsToMany(Classroom::class);
+    }
 }
