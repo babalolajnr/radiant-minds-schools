a5e1e15ff87fdc47b68afdbab6b5c6e2f658a10c
babalolajnr <babalolajnr@gmail.com>
2021-01-06 16:06:10 +0100

    added first_name, last_name and user_type to test_new_users_can_register method

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -20,7 +20,9 @@ class RegistrationTest extends TestCase
     public function test_new_users_can_register()
     {
         $response = $this->post('/register', [
-            'name' => 'Test User',
+            'first_name' => 'Test User',
+            'last_name' => 'Test User',
+            'user_type' => 'admin',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
