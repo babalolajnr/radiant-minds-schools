c6d97c20e0cfcaf71860d08e1812f854c625645a
babalolajnr <babalolajnr@gmail.com>
2021-01-06 16:39:03 +0100

    created guradian model ...

---
+++ database/factories/GuardianFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Guardian;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class GuardianFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Guardian::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
