c58827e0ea36dc2bb720b900898e16a7a8bb5259
babalolajnr <babalolajnr@gmail.com>
2021-01-07 07:53:07 +0100

    added new fields

--- database/migrations/2021_01_06_155659_create_teachers_table.php
+++ database/migrations/2021_01_06_155659_create_teachers_table.php
@@ -15,6 +15,12 @@ class CreateTeachersTable extends Migration
     {
         Schema::create('teachers', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('status');
+            $table->string('date_of_birth');
             $table->timestamps();
         });
     }
