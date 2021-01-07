41e6d68a7aecdd5a6e4f81bffb1266875eee3810
babalolajnr <babalolajnr@gmail.com>
2021-01-07 14:11:01 +0100

    added create teacher route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +21,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->group( function () {
+    Route::get('/create/teacher', [TeacherController::class, 'create']);
+});
 
 require __DIR__.'/auth.php';
