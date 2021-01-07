f903a36a99f6cff8eddace81c04d96ea80966e6e
babalolajnr <babalolajnr@gmail.com>
2021-01-07 14:47:38 +0100

    added admin can store a new teacher test

--- tests/Feature/TeacherTest.php
+++ tests/Feature/TeacherTest.php
@@ -12,13 +12,25 @@ use Tests\TestCase;
 class TeacherTest extends TestCase
 {
     use WithFaker;
-    // use RefreshDatabase;
+    use RefreshDatabase;
 
-    public function test_admin_can_access_create_view()
+    public function test_admin_can_access_create_teacher_view()
     {
         $user = User::factory()->create(['user_type' => 'admin']);
         $request = $this->actingAs($user)->get('/create/teacher');
+        $request->assertStatus(200);
+    }
 
+    public function test_admin_can_store_a_new_teacher()
+    {
+        $user = User::factory()->create(['user_type' => 'admin']);
+        $request = $this->actingAs($user)->post('/store/teacher', [
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'email' => $this->faker->email,
+            'phone' => '08124792224',
+            'date_of_birth' => $this->faker->dateTimeThisCentury(),
+        ]);
         $request->assertStatus(200);
     }
 }
