0f08665a2eef4039f8437c25d03f49bce7a0f74a
babalolajnr <babalolajnr@gmail.com>
2021-01-07 16:32:56 +0100

    added teacher factory definition

--- database/factories/TeacherFactory.php
+++ database/factories/TeacherFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Teacher;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class TeacherFactory extends Factory
 {
@@ -21,8 +22,18 @@ class TeacherFactory extends Factory
      */
     public function definition()
     {
+        $firstName = $this->faker->firstName;
+        $lastName = $this->faker->lastName;
+        $fullname = $firstName . ' ' . $lastName;
+        $slug = Str::of($fullname)->slug('-');
         return [
-            //
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'slug' => $slug,
+            'email' => $this->faker->email,
+            'phone' => '08124792224',
+            'status' => $this->faker->randomElement(['active', 'inactive', 'susended']),
+            'date_of_birth' => $this->faker->dateTimeThisCentury(),
         ];
     }
 }
