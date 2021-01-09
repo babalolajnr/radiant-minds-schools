d01a142b93242e6ca39caa58a6dec15152960bbf
babalolajnr <babalolajnr@gmail.com>
2021-01-09 11:28:24 +0100

    created student policy

---
+++ app/Policies/StudentPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Student;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class StudentPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Student  $student
+     * @return mixed
+     */
+    public function view(User $user, Student $student)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Student  $student
+     * @return mixed
+     */
+    public function update(User $user, Student $student)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Student  $student
+     * @return mixed
+     */
+    public function delete(User $user, Student $student)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Student  $student
+     * @return mixed
+     */
+    public function restore(User $user, Student $student)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Student  $student
+     * @return mixed
+     */
+    public function forceDelete(User $user, Student $student)
+    {
+        //
+    }
+}
