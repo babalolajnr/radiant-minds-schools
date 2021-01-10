6b6314e059ff3a7d4b5e2c4709c4da96b162706a
babalolajnr <babalolajnr@gmail.com>
2021-01-10 14:48:59 +0100

    added guardian factory method

--- database/factories/GuardianFactory.php
+++ database/factories/GuardianFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Guardian;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class GuardianFactory extends Factory
 {
@@ -21,8 +22,28 @@ class GuardianFactory extends Factory
      */
     public function definition()
     {
+        $firstName = $this->faker->firstName;
+        $lastName = $this->faker->lastName;
+        $title = $this->faker->title;
+        $fullname = $title . ' ' . $firstName . ' ' . $lastName;
+        $checkIfTaken = Guardian::where('full_name', $fullname)->first();
+
+        if (is_null($checkIfTaken)) {
+            $fullname = $fullname;
+        } else {
+            do {
+                $fullname = $fullname . Str::random(3);
+                $checkIfTaken = Guardian::where('full_name', $fullname)->first();
+            } while (!is_null($checkIfTaken));
+        }
+
         return [
-            //
+            'title' => $title,
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'email' => $this->faker->email,
+            'phone' => $this->faker->e164PhoneNumber,
+            'full_name' => $fullname,
         ];
     }
 }
