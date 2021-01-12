fd2a5a3545fb80bc148c075fbf0dce8c25897595
babalolajnr <babalolajnr@gmail.com>
2021-01-12 09:08:43 +0100

    removed slug field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('user_type');
-            $table->string('slug');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
