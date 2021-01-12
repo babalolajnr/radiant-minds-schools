91ca24993147da366c8377a3caf78a4561d87106
babalolajnr <babalolajnr@gmail.com>
2021-01-12 17:09:35 +0100

    used softdeletes

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Student extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
 }
