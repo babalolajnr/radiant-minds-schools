e6bd72b246a4250456ee53ebabc0882f536a5ec2
babalolajnr <babalolajnr@gmail.com>
2021-01-12 21:42:23 +0100

    added index method

--- app/Http/Controllers/ClassroomController.php
+++ app/Http/Controllers/ClassroomController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Classroom;
 use Illuminate\Http\Request;
 
 class ClassroomController extends Controller
 {
-    //
+    public function index()
+    {
+        $classrooms = Classroom::all();
+        return response(200);
+    }
 }
