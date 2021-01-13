31cdb3aa52e73c32047afd03cc5a77a20f5db4c3
babalolajnr <babalolajnr@gmail.com>
2021-01-13 17:31:01 +0100

    added seeder logic

--- database/seeders/AssessmentTypeSeeder.php
+++ database/seeders/AssessmentTypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\AssessmentType;
 use Illuminate\Database\Seeder;
 
 class AssessmentTypeSeeder extends Seeder
@@ -13,6 +14,16 @@ class AssessmentTypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $assessmentTypes = [
+            ['name' => 'C.A', 'max_score' => 40],
+            ['name' => 'Exam', 'max_score' => 100],
+        ];
+
+        foreach ($assessmentTypes as $assessmentType) {
+            AssessmentType::create([
+                'name' => $assessmentType['name'],
+                'max_score' => $assessmentType['max_score']
+            ]);
+        }
     }
 }
