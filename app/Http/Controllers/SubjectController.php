4f8c121496478ffaebe1fd1ac2cf45aad08eb503
babalolajnr <babalolajnr@gmail.com>
2021-01-13 15:07:29 +0100

    added subject controllermethods

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subject;
 use Illuminate\Http\Request;
 
 class SubjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $subjects = Subject::all();
+        return response(200);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => ['required', 'string', 'unique:subjects']
+        ]);
+
+        Subject::create($request->all());
+        return response(200);
+    }
+
+    public function edit($id)
+    {
+
+        $subject = Subject::findOrFail($id);
+        return response(200);
+    }
+
+    public function update($id, Request $request)
+    {
+        $subject = Subject::findOrFail($id);
+        $subject->update($request->all());
+        return response(200);
+    }
+
+    public function destroy($id, Subject $subject)
+    {
+        $this->authorize('delete', $subject);
+        $subject = Subject::findOrFail($id);
+        $subject->delete();
+        return response(200);
+    }
 }
