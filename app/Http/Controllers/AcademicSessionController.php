8d43cca8c1a326dd07b9f731bf4419a81dcdb22f
babalolajnr <babalolajnr@gmail.com>
2021-01-13 21:06:22 +0100

    added controller methods

--- app/Http/Controllers/AcademicSessionController.php
+++ app/Http/Controllers/AcademicSessionController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AcademicSession;
 use Illuminate\Http\Request;
 
 class AcademicSessionController extends Controller
 {
-    //
+    public function index()
+    {
+        $academicSessions = AcademicSession::all();
+        return response(200);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => ['required', 'string', 'unique:academic_sessions']
+        ]);
+
+        AcademicSession::create($request->all());
+        return response(200);
+    }
+
+    public function edit($id)
+    {
+        $academicSession = AcademicSession::findOrFail($id);
+        return response(200);
+    }
+
+    public function update($id, Request $request)
+    {
+        $academicSession = AcademicSession::findOrFail($id);
+        $academicSession->update($request->all());
+        return response(200);
+    }
+
+    // public function destroy($id, AcademicSession $academicSession)
+    // {
+    //     $this->authorize('delete', $academicSession);
+    //     $academicSession = AcademicSession::findOrFail($id);
+    //     $academicSession->delete();
+    //     return response(200);
+    // }
 }
