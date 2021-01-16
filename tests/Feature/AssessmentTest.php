8f638a18eb9d9a3bbc8994976cae465b98a31141
babalolajnr <babalolajnr@gmail.com>
2021-01-16 14:05:43 +0100

    added user can create assessment test

--- tests/Feature/AssessmentTest.php
+++ tests/Feature/AssessmentTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\AcademicSession;
+use App\Models\AssessmentType;
+use App\Models\Term;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -9,13 +12,24 @@ use Tests\TestCase;
 
 class AssessmentTest extends TestCase
 {
+    use RefreshDatabase;
     public function test_user_can_get_assessments()
     {
         $user = User::factory()->create();
         $response = $this->actingAs($user)->get('/assessments');
         $response->assertStatus(200);
     }
-    // public function test_user_can_create_assessment()
-    // {
-    // }
+
+    public function test_user_can_create_assessment()
+    {
+        $this->withoutExceptionHandling();
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->post('/store/assessment', [
+            'term' => Term::factory()->create()->name,
+            'assessment_type' => AssessmentType::factory()->create()->name,
+            'academic_session' => AcademicSession::factory()->create()->name, 
+        ]);
+
+        $response->assertStatus(200);
+    }
 }
