bb97f995380358b8ebb60a52befc58150c5409e0
babalolajnr <babalolajnr@gmail.com>
2021-01-16 18:38:43 +0100

    added assessment result store function

--- app/Http/Controllers/AssessmentResultController.php
+++ app/Http/Controllers/AssessmentResultController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Assessment;
+use App\Models\AssessmentResult;
+use App\Models\Student;
+use App\Models\Subject;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class AssessmentResultController extends Controller
 {
-    //
+    public function store(Request $request, $studentID, $subjectID)
+    {
+        $student = Student::findOrFail($studentID);
+        $subject = Subject::findOrFail($subjectID);
+        
+        if (!$request->assessment) {
+            throw ValidationException::withMessages(['assessment' => 'Assessment is required']);
+        }
+
+        $assessment = Assessment::where('name', $request->assessment)->first();
+        $assessmentMaxScore = $assessment->assessmentType->max_score;
+
+        $messages = [
+            'between' => 'The score must be between 0 and ' . $assessmentMaxScore
+        ];
+
+        $this->validate($request, [
+            'mark' => ['required', 'numeric', 'between:0,' . $assessmentMaxScore],
+            'assessment' => ['string']
+        ], $messages);
+
+        AssessmentResult::create([
+            'assessment_id' => $assessment->id,
+            'subject_id' => $subject->id,
+            'student_id' => $student->id,
+            'mark' => $request->mark
+        ]);
+
+    }
 }
