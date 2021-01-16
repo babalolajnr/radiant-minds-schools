5a12ec319683155ff73c1d2041b91cef95f30126
babalolajnr <babalolajnr@gmail.com>
2021-01-16 18:42:30 +0100

    added user can store assessment result route

--- tests/Feature/AssessmentResultTest.php
+++ tests/Feature/AssessmentResultTest.php
@@ -2,20 +2,30 @@
 
 namespace Tests\Feature;
 
+use App\Models\Assessment;
+use App\Models\Student;
+use App\Models\Subject;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AssessmentResultTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    // use RefreshDatabase;
+    
+    public function test_user_can_store_assessment()
     {
-        $response = $this->get('/');
+        $this->withoutExceptionHandling();
+        $user = User::factory()->create();
+        $student = Student::factory()->create();
+        $subject = Subject::factory()->create();
+        $assessment =  Assessment::factory()->create();
+
+        $response = $this->actingAs($user)->post('/assessment-result/' . $student->id . '/' . $subject->id, [
+            'mark' => mt_rand(0, $assessment->assessmentType->max_score), 
+            'assessment' => $assessment->name
+        ]);
 
         $response->assertStatus(200);
     }
