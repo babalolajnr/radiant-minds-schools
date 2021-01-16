2f7209710677227fe3232ddc1183fc951161189a
babalolajnr <babalolajnr@gmail.com>
2021-01-16 12:34:18 +0100

    added user can get assessments test

--- tests/Feature/AssessmentTest.php
+++ tests/Feature/AssessmentTest.php
@@ -2,21 +2,20 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AssessmentTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    public function test_user_can_get_assessments()
     {
-        $response = $this->get('/');
-
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/assessments');
         $response->assertStatus(200);
     }
+    // public function test_user_can_create_assessment()
+    // {
+    // }
 }
