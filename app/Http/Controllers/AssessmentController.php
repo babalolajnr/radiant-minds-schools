6e3918410e03ce198a7f2cf4c15407991c038613
babalolajnr <babalolajnr@gmail.com>
2021-01-16 12:28:26 +0100

    added assessment controller index method

--- app/Http/Controllers/AssessmentController.php
+++ app/Http/Controllers/AssessmentController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Assessment;
 use Illuminate\Http\Request;
 
 class AssessmentController extends Controller
 {
-    //
+    public function index()
+    {
+        $assessments = Assessment::all();
+        return response(200);
+    }
 }
