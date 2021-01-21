72c9a61516bb380d0167d8c2884adad324ccaa05
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-01-21 14:55:17 +0100

    added my website link to footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
-<footer class="footer">
-    <div class="container-fluid clearfix">
-        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
-        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates</a> from Bootstrapdash.com</span>
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 1.0.0
     </div>
+    <strong>Powered by <a href="https://babalolajnr.netlify.app" target="_blank">Babalola Abdulqudduus</a> &copy; <span> 2020 </span>
 </footer>
