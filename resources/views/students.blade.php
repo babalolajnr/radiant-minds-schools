50fd0cc6557b9aba9e3b1611c7e039229975a022
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-01-22 12:57:36 +0100

    created students view

---
+++ resources/views/students.blade.php
@@ -0,0 +1,206 @@
+<x-app-layout>
+    <x-slot name="styles">
+        <!-- Toastr -->
+        <link rel="stylesheet" href="{{ asset('TAssets/plugins/toastr/toastr.min.css') }}">
+        <!-- DataTables -->
+        <link rel="stylesheet"
+            href="{{ asset('TAssets/plugins/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+        <link rel="stylesheet"
+            href="{{ asset('TAssets/plugins/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+        <link rel="stylesheet"
+            href="{{ asset('TAssets/plugins/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+    </x-slot>
+
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <span id="success" {{ session('success') ? 'data-success = true' : false }}
+            data-success-message='{{ json_encode(session('success')) }}'></span>
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Students</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="#">Layout</a></li>
+                            <li class="breadcrumb-item active">Fixed Layout</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+
+                        <!-- Default box -->
+
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Students</h3>
+                            </div>
+                            <div class="card-body">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>S/N</th>
+                                            <th>Admission No</th>
+                                            <th>First name</th>
+                                            <th>Last name</th>
+                                            <th>Sex</th>
+                                            <th>Class</th>
+                                            <th>Guardian</th>
+                                            <th>Status</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                            $no = 1;
+                                        ?>
+                                        @foreach($students as $student)
+                                        <tr>
+                                            <td>
+                                                <?php
+                                                    echo $no;
+                                                    $no++;
+                                                ?>
+                                            </td>
+                                            <td>{{ $student->admission_no }}</td>
+                                            <td>
+                                                {{ $student->first_name }}
+                                            </td>
+                                            <td>
+                                                {{ $student->last_name }}
+                                            </td>
+                                            <td>
+                                                {{ $student->sex }}
+                                            </td>
+                                            <td>
+                                                {{ $student->classroom->name }}
+                                            </td>
+                                            <td>
+                                                {{ $student->guardian->title. ' '. $student->guardian->first_name. ' '. $student->guardian->last_name }}
+                                            </td>
+                                            <td>{{ $student->status }}</td>
+                                            <td>
+                                                <div class="btn-group">
+                                                    <a href="edit/student/{{ $student->id }}">
+                                                        <button type="button" class="btn btn-default btn-flat"
+                                                            title="Edit">
+                                                            <i class="fa fa-edit"></i>
+                                                        </button>
+                                                    </a>
+                                                    {{-- render if user is authorized to delete --}}
+                                                    @can('delete', $student)
+                                                    <form action="/delete/student/{{ $student->id }}" method="POST">
+                                                        @method('DELETE')
+                                                        @csrf
+                                                        <button type="submit" class="btn btn-default btn-flat"
+                                                            title="Delete">
+                                                            <i class="fas fa-trash"></i>
+                                                        </button>
+                                                    </form>
+                                                    @endcan
+
+                                                    {{-- render if user is not authorized to delete --}}
+                                                    @cannot('delete', $student)
+                                                    <button type="submit" class="btn btn-default btn-flat"
+                                                        title="Delete" disabled>
+                                                        <i class="fas fa-trash"></i>
+                                                    </button>
+                                                    @endcannot
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>S/N</th>
+                                            <th>Admission No</th>
+                                            <th>First name</th>
+                                            <th>Last name</th>
+                                            <th>Sex</th>
+                                            <th>Class</th>
+                                            <th>Guardian</th>
+                                            <th>Status</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </section>
+        <!-- /.content -->
+    </div>
+
+    <x-slot name="scripts">
+        <!-- Toastr -->
+        <script src="{{ asset('TAssets/plugins/toastr/toastr.min.js') }}"></script>
+        <!-- DataTables  & Plugins -->
+        <script src="{{ asset('TAssets/plugins/datatables/jquery.dataTables.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/jszip/jszip.min.js') }}"></script>
+        <script src="{{ asset('TAssets/plugins/pdfmake/pdfmake.min.js') }}"></script>
+        <script src="{{ asset('TAssets/plugins/pdfmake/vfs_fonts.js') }}"></script>
+        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/buttons.html5.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/buttons.print.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/buttons.colVis.min.js') }}">
+        </script>
+        <!-- AdminLTE App -->
+        <script>
+            $(function () {
+                let Success = document.getElementById('success')
+                // if data-success = 'true' display alert
+                if (Success.dataset.success == 'true')
+                    $(document).Toasts('create', {
+                        class: 'bg-success',
+                        title: 'Success',
+                        subtitle: 'Close',
+                        body: JSON.parse(Success.dataset.successMessage)
+                    })
+
+            });
+            $(function () {
+                $("#example1").DataTable({
+                    "responsive": true,
+                    "lengthChange": false,
+                    "autoWidth": false,
+                    "buttons": ["copy", "csv", "excel", "pdf", "print"]
+                }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+                // $('#example2').DataTable({
+                //     "paging": true,
+                //     "lengthChange": false,
+                //     "searching": false,
+                //     "ordering": true,
+                //     "info": true,
+                //     "autoWidth": false,
+                //     "responsive": true,
+                // });
+            });
+
+        </script>
+    </x-slot>
+</x-app-layout>
