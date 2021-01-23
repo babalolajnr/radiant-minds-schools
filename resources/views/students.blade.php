58288a691e1256bd6e48823b4b65f852cd599190
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-01-23 15:09:44 +0100

    feature: added student details modal

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -92,6 +92,12 @@
                                             <td>{{ $student->status }}</td>
                                             <td>
                                                 <div class="btn-group">
+                                                    <button type="button" id="viewStudentButton"
+                                                        data-admission-no={{ $student->admission_no }}
+                                                        class="btn btn-default btn-flat"
+                                                        onclick="showViewModal({{ $student }})" title="View">
+                                                        <i class="fas fa-eye"></i>
+                                                    </button>
                                                     <a href="edit/student/{{ $student->id }}">
                                                         <button type="button" class="btn btn-default btn-flat"
                                                             title="Edit">
@@ -142,6 +148,186 @@
                 </div>
         </section>
         <!-- /.content -->
+
+        <div class="modal fade" id="viewModal">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Student Details</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-3">
+
+                                    <!-- Profile Image -->
+                                    <div class="card card-primary card-outline">
+                                        <div class="card-body box-profile">
+                                            <div class="text-center">
+                                                <img class="profile-user-img img-fluid img-circle"
+                                                    src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
+                                            </div>
+
+                                            <h3 class="profile-username text-center">Nina Mcintire</h3>
+
+                                            <p class="text-muted text-center" id="admissionNo">Software Engineer</p>
+
+                                        </div>
+                                        <!-- /.card-body -->
+                                    </div>
+                                    <!-- /.card -->
+
+                                </div>
+                                <!-- /.col -->
+                                <div class="col-md-9">
+                                    <div class="card">
+                                        <div class="card-header p-2">
+                                            <ul class="nav nav-pills">
+                                                <li class="nav-item"><a class="nav-link active" href="#about"
+                                                        data-toggle="tab">About</a></li>
+                                                <li class="nav-item"><a class="nav-link" href="#timeline"
+                                                        data-toggle="tab">Timeline</a></li>
+                                                <li class="nav-item"><a class="nav-link" href="#settings"
+                                                        data-toggle="tab">Settings</a></li>
+                                            </ul>
+                                        </div><!-- /.card-header -->
+                                        <div class="card-body">
+                                            <div class="tab-content">
+                                                <div class="active tab-pane" id="about">
+                                                    <strong></i>Class</strong>
+
+                                                    <p class="text-muted" id="classroom">
+
+                                                    </p>
+
+                                                    <hr>
+
+                                                    <strong></i>Local
+                                                        government</strong>
+
+                                                    <p class="text-muted" id="lg"></p>
+
+                                                    <hr>
+
+                                                    <strong></i>State</strong>
+
+                                                    <p class="text-muted" id="state"></p>
+                                                    <hr>
+
+                                                    <strong></i>Country</strong>
+
+                                                    <p class="text-muted" id="country"></p>
+                                                    <hr>
+
+                                                    <strong></i>Date of birth</strong>
+
+                                                    <p class="text-muted" id="dob"></p>
+
+                                                    <hr>
+
+                                                    <strong></i>Place of birth</strong>
+
+                                                    <p class="text-muted" id="pob"></p>
+
+                                                    <hr>
+                                                    <strong></i>Blood Group</strong>
+
+                                                    <p class="text-muted" id="bloodGroup"></p>
+
+                                                    <hr>
+                                                    <strong></i>status</strong>
+
+                                                    <p class="text-muted" id="status"></p>
+
+                                                    <hr>
+
+                                                </div>
+                                                <!-- /.tab-pane -->
+                                                <div class="tab-pane" id="timeline">
+                                                    
+                                                </div>
+                                                <!-- /.tab-pane -->
+
+                                                <div class="tab-pane" id="settings">
+                                                    <form class="form-horizontal">
+                                                        <div class="form-group row">
+                                                            <label for="inputName"
+                                                                class="col-sm-2 col-form-label">Name</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="email" class="form-control" id="inputName"
+                                                                    placeholder="Name">
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <label for="inputEmail"
+                                                                class="col-sm-2 col-form-label">Email</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="email" class="form-control" id="inputEmail"
+                                                                    placeholder="Email">
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <label for="inputName2"
+                                                                class="col-sm-2 col-form-label">Name</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="text" class="form-control" id="inputName2"
+                                                                    placeholder="Name">
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <label for="inputExperience"
+                                                                class="col-sm-2 col-form-label">Experience</label>
+                                                            <div class="col-sm-10">
+                                                                <textarea class="form-control" id="inputExperience"
+                                                                    placeholder="Experience"></textarea>
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <label for="inputSkills"
+                                                                class="col-sm-2 col-form-label">Skills</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="text" class="form-control" id="inputSkills"
+                                                                    placeholder="Skills">
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <div class="offset-sm-2 col-sm-10">
+                                                                <div class="checkbox">
+                                                                    <label>
+                                                                        <input type="checkbox"> I agree to the <a
+                                                                            href="#">terms and conditions</a>
+                                                                    </label>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <div class="offset-sm-2 col-sm-10">
+                                                                <button type="submit"
+                                                                    class="btn btn-danger">Submit</button>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                                <!-- /.tab-pane -->
+                                            </div>
+                                            <!-- /.tab-content -->
+                                        </div><!-- /.card-body -->
+                                    </div>
+                                    <!-- /.card -->
+                                </div>
+                                <!-- /.col -->
+                            </div>
+                            <!-- /.row -->
+                        </div>
+                    </div>
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <!-- /.modal-dialog -->
+        </div>
     </div>
 
     <x-slot name="scripts">
@@ -171,6 +357,20 @@
         </script>
         <!-- AdminLTE App -->
         <script>
+            function showViewModal(data) {
+                let fullname = data.first_name + ' ' + data.last_name
+                $('#viewModal .profile-username').html(fullname)
+                $('#viewModal #admissionNo').html(data.admission_no)
+                $('#viewModal #classroom').html(data.classroom.name)
+                $('#viewModal #lg').html(data.lg)
+                $('#viewModal #state').html(data.state)
+                $('#viewModal #dob').html(data.date_of_birth)
+                $('#viewModal #pob').html(data.place_of_birth)
+                $('#viewModal #bloodGroup').html(data.blood_group)
+                $('#viewModal #status').html(data.status)
+                $('#viewModal').modal('show')
+                console.log(data)
+            }
             $(function () {
                 let Success = document.getElementById('success')
                 // if data-success = 'true' display alert
