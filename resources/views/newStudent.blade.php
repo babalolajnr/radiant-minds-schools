23f70a2515c6b3bdae81a738e07e6987bf631311
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-01-25 15:56:08 +0100

    changed main body title to "New Student"

--- resources/views/newStudent.blade.php
+++ resources/views/newStudent.blade.php
@@ -13,7 +13,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Academic Sessions</h1>
+                        <h1>New Student</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
