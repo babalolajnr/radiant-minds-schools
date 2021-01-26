6a83093d132b8fe200601a504fbbc02f94d2cdf1
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-01-26 08:55:00 +0100

    fixed unique validation rule

--- app/Http/Controllers/GuardianController.php
+++ app/Http/Controllers/GuardianController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Guardian;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class GuardianController extends Controller
 {
@@ -30,8 +31,8 @@ class GuardianController extends Controller
             'title' => ['required', 'max:30', 'string'],
             'first_name' => ['required', 'max:30', 'string'],
             'last_name' => ['required', 'max:30', 'string'],
-            'email' => ['required', 'string', 'email:rfc,dns', 'unique:guardians'],
-            'phone' => ['required', 'string', 'between:10,15', 'unique:guardians'],
+            'email' => ['required', 'string', 'email:rfc,dns', Rule::unique('guardians')->ignore($guardian)],
+            'phone' => ['required', 'string', 'between:10,15', Rule::unique('guardians')->ignore($guardian)],
             'occupation' => ['required', 'string'],
             'address' => ['required']
         ]);
