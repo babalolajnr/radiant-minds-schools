c62e579e91f465e7a5f4276dda0cf2cf723535e9
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-01-26 08:05:54 +0100

    modified edit method

--- app/Http/Controllers/GuardianController.php
+++ app/Http/Controllers/GuardianController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Guardian;
 use Illuminate\Http\Request;
 
 class GuardianController extends Controller
 {
-    public function edit()
+    public function edit($guardian)
     {
-        return view('editGuardian');
+        $guardian = Guardian::where('phone', $guardian);
+        if (!$guardian->exists()) {
+            abort(404);
+        } 
+
+        $guardian = $guardian->first();
+        return view('editGuardian', compact(['guardian']));
     }
 }
