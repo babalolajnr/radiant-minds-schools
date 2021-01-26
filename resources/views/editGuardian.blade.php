d8225823fdd92a2bf45f411de63e0551599295a0
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-01-26 08:05:30 +0100

    added edit guardian form

---
+++ resources/views/editGuardian.blade.php
@@ -0,0 +1,160 @@
+<x-app-layout>
+    <x-slot name="styles">
+        <!-- Toastr -->
+        <link rel="stylesheet" href="{{ asset('TAssets/plugins/toastr/toastr.min.css') }}">
+        <!-- Select2 -->
+        <link rel="stylesheet" href="{{ asset('TAssets/plugins/select2/css/select2.min.css') }}">
+        <link rel="stylesheet"
+            href="{{ asset('TAssets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css"') }}">
+    </x-slot>
+    <div class=" content-wrapper">
+        <!-- Content Header (Page header) -->
+        <span id="success" {{ session('success') ? 'data-success = true' : false }}
+            data-success-message='{{ json_encode(session('success')) }}'></span>
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Edit Guardian
+                            ({{ $guardian->title . ' ' . $guardian->first_name . ' ' . $guardian->last_name}})</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="#">Layout</a></li>
+                            <li class="breadcrumb-item active">Fixed Layout</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <!-- Default box -->
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Edit Guardian</h3>
+                            </div>
+                            <form id="updateStudent" method="POST" action="/update/student/{{ $guardian->id }}">
+                                @csrf
+                                @method('PATCH')
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label>Title</label>
+                                        <input type="text" name="title"
+                                            class="form-control @error('title') is-invalid @enderror"
+                                            value="{{ old('title', $guardian->title) }}">
+                                        @error('title')
+                                        <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label>First name</label>
+                                        <input type=" text" name="first_name"
+                                            class="form-control @error('first_name') is-invalid @enderror"
+                                            value="{{ old('first_name', $guardian->first_name) }}">
+                                        @error('first_name')
+                                        <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Last name</label>
+                                        <input type=" text" name="last_name"
+                                            class="form-control @error('last_name') is-invalid @enderror"
+                                            value="{{ old('last_name', $guardian->last_name) }}">
+                                        @error('last_name')
+                                        <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Email address</label>
+                                        <input type="email" name="email"
+                                            class="form-control @error('email') is-invalid @enderror"
+                                            value="{{ old('email', $guardian->email) }}">
+                                        @error('email')
+                                        <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Phone number</label>
+                                        <input type=" text" name="phone"
+                                            class="form-control @error('phone') is-invalid @enderror"
+                                            value="{{ old('phone', $guardian->phone) }}">
+                                        @error('phone')
+                                        <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Occupation</label>
+                                        <input type=" text" name="occupation"
+                                            class="form-control @error('occupation') is-invalid @enderror"
+                                            value="{{ old('occupation', $guardian->occupation) }}">
+                                        @error('occupation')
+                                        <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Address</label>
+                                        <input type=" text" name="address"
+                                            class="form-control @error('address') is-invalid @enderror"
+                                            value="{{ old('address', $guardian->address) }}">
+                                        @error('address')
+                                        <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+
+
+                    </div>
+                </div>
+        </section>
+        <!-- /.content -->
+    </div>
+    <x-slot name="scripts">
+        <!-- Select2 -->
+        <script src="{{ asset('TAssets/plugins/select2/js/select2.full.min.js') }}"></script>
+        <!-- InputMask -->
+        <script src="{{ asset('TAssets/plugins/moment/moment.min.js') }}"></script>
+        <script src="{{ asset('TAssets/plugins/inputmask/jquery.inputmask.min.js') }}"></script>
+        <script>
+            $(function () {
+                //Initialize Select2 Elements
+                $('.select2').select2()
+                $('#datemask').inputmask('yyyy-mm-dd', {
+                    'placeholder': 'yyyy-mm-dd'
+                })
+                //Money Euro
+                $('[data-mask]').inputmask()
+
+            })
+
+            //launch toastr 
+            $(function () {
+                let Success = document.getElementById('success')
+                // if data-success = 'true' display alert
+                if (Success.dataset.success == 'true')
+                    $(document).Toasts('create', {
+                        class: 'bg-success',
+                        title: 'Success',
+                        subtitle: 'Close',
+                        body: JSON.parse(Success.dataset.successMessage)
+                    })
+
+            });
+
+        </script>
+    </x-slot>
+</x-app-layout>
