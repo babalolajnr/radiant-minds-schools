6ee2fe58118b73e792bba7f87f10675bcb80f409
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-01-27 16:07:04 +0100

    added error toastr

--- resources/views/academicSession.blade.php
+++ resources/views/academicSession.blade.php
@@ -15,6 +15,8 @@
         <!-- Content Header (Page header) -->
         <span id="success" {{ session('success') ? 'data-success = true' : false }}
             data-success-message='{{ json_encode(session('success')) }}'></span>
+        <span id="error" {{ session('error') ? 'data-error = true' : false }}
+            data-error-message='{{ json_encode(session('error')) }}'></span>
         <section class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
@@ -92,16 +94,16 @@
                                                     </a>
                                                     {{-- render if user is authorized to delete --}}
                                                     @can('delete', $academicSession)
-                                                    <button type="button" class="btn btn-danger btn-flat"
-                                                        title="Delete" onclick="deleteConfirmationModal({{ $academicSession }})">
+                                                    <button type="button" class="btn btn-danger btn-flat" title="Delete"
+                                                        onclick="deleteConfirmationModal({{ $academicSession }})">
                                                         <i class="fas fa-trash"></i>
                                                     </button>
                                                     @endcan
 
                                                     {{-- render if user is not authorized to delete --}}
                                                     @cannot('delete', $academicSession)
-                                                    <button type="submit" class="btn btn-danger btn-flat"
-                                                        title="Delete" disabled>
+                                                    <button type="submit" class="btn btn-danger btn-flat" title="Delete"
+                                                        disabled>
                                                         <i class="fas fa-trash"></i>
                                                     </button>
                                                     @endcannot
@@ -186,6 +188,8 @@
 
             $(function () {
                 let Success = document.getElementById('success')
+                let Error = document.getElementById('error')
+
                 // if data-success = 'true' display alert
                 if (Success.dataset.success == 'true')
                     $(document).Toasts('create', {
@@ -194,6 +198,14 @@
                         subtitle: 'Close',
                         body: JSON.parse(Success.dataset.successMessage)
                     })
+                if (Error.dataset.error == 'true')
+                    $(document).Toasts('create', {
+                        class: 'bg-danger',
+                        title: 'Error',
+                        subtitle: 'Close',
+                        body: JSON.parse(Error.dataset.errorMessage)
+                    })
+
 
             });
             $(function () {
