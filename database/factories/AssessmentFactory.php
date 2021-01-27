d3cd50acbac7f4a54e20e1e6511bd9844c0cd983
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-01-27 16:04:49 +0100

    refactored assessment factory

--- database/factories/AssessmentFactory.php
+++ database/factories/AssessmentFactory.php
@@ -7,6 +7,8 @@ use App\Models\Assessment;
 use App\Models\AssessmentType;
 use App\Models\Term;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Artisan;
 
 class AssessmentFactory extends Factory
 {
@@ -16,6 +18,49 @@ class AssessmentFactory extends Factory
      * @var string
      */
     protected $model = Assessment::class;
+    private function generateValues()
+    {
+        $term = Term::pluck('id')->all();
+        $assessmentType = AssessmentType::pluck('id')->all();
+        $academicSession = AcademicSession::pluck('id')->all();
+
+        if (!empty($assessmentType)) {
+            $assessmentType = Arr::random($assessmentType);
+        } else {
+            Artisan::call('db:seed', ['--class' => 'AssessmentTypeSeeder']);
+            $assessmentType = AssessmentType::pluck('id')->all();
+            $assessmentType = Arr::random($assessmentType);
+        }
+
+        if (!empty($term)) {
+            $term = Arr::random($term);
+        } else {
+            Artisan::call('db:seed', ['--class' => 'TermSeeder']);
+            $term = Term::pluck('id')->all();
+            $term = Arr::random($term);
+        }
+
+        if (!empty($academicSession)) {
+            $academicSession = Arr::random($academicSession);
+        } else {
+            Artisan::call('db:seed', ['--class' => 'AcademicSessionSeeder']);
+            $academicSession = AcademicSession::pluck('id')->all();
+            $academicSession = Arr::random($academicSession);
+        }
+
+        $assessmentTypeName = AssessmentType::find($assessmentType)->name;
+        $termName = Term::find($term)->name;
+        $academicSessionName = AcademicSession::find($academicSession)->name;
+
+        return [
+            'term' => $term,
+            'academicSession' => $academicSession,
+            'assessmentType' => $assessmentType,
+            'assessmentTypeName' => $assessmentTypeName,
+            'termName'  => $termName,
+            'academicSessionName' => $academicSessionName
+        ];
+    }
 
     /**
      * Define the model's default state.
@@ -24,14 +69,22 @@ class AssessmentFactory extends Factory
      */
     public function definition()
     {
-        $term = Term::factory()->create();
-        $assessmentType = AssessmentType::factory()->create();
-        $academicSession = AcademicSession::factory()->create();
-        $name = $assessmentType->name . ' ' . $term->name . ' ' . $academicSession->name;
+
+
+        $values = $this->generateValues();
+        $name = $values['assessmentTypeName'] . ' ' . $values['termName'] . ' ' . $values['academicSessionName'];
+        $assessment = Assessment::where('name', $name);
+        
+        while ($assessment->exists()) {
+            $values = $this->generateValues();
+            $name = $values['assessmentTypeName'] . ' ' . $values['termName'] . ' ' . $values['academicSessionName'];
+            $assessment = Assessment::where('name', $name);
+        }
+
         return [
-            'term_id' => $term->id,
-            'assessment_type_id' => $assessmentType->id,
-            'academic_session_id' => $academicSession->id,
+            'term_id' => $values['term'],
+            'assessment_type_id' => $values['assessmentType'],
+            'academic_session_id' => $values['academicSession'],
             'name' => $name
         ];
     }
