a45e61367b8d120289299e0faecac7daaec5207f
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-01-27 16:06:10 +0100

    added unique constraint to name field

--- database/migrations/2021_01_06_165649_create_terms_table.php
+++ database/migrations/2021_01_06_165649_create_terms_table.php
@@ -15,7 +15,7 @@ class CreateTermsTable extends Migration
     {
         Schema::create('terms', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
