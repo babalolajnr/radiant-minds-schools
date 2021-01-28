fb3525b49d1e323b736ed06920c663d1090094bd
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-01-28 22:21:32 +0100

    added get assessment results form

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -212,8 +212,8 @@
                                                         data-toggle="tab">About</a></li>
                                                 <li class="nav-item"><a class="nav-link" href="#guardianInfo"
                                                         data-toggle="tab">Guardian</a></li>
-                                                <li class="nav-item"><a class="nav-link" href="#settings"
-                                                        data-toggle="tab">Settings</a></li>
+                                                <li class="nav-item"><a class="nav-link" href="#results"
+                                                        data-toggle="tab">Results</a></li>
                                             </ul>
                                         </div><!-- /.card-header -->
                                         <div class="card-body">
@@ -289,63 +289,28 @@
                                                 </div>
                                                 <!-- /.tab-pane -->
 
-                                                <div class="tab-pane" id="settings">
-                                                    <form class="form-horizontal">
-                                                        <div class="form-group row">
-                                                            <label for="inputName"
-                                                                class="col-sm-2 col-form-label">Name</label>
-                                                            <div class="col-sm-10">
-                                                                <input type="email" class="form-control" id="inputName"
-                                                                    placeholder="Name">
-                                                            </div>
+                                                <div class="tab-pane" id="results">
+                                                    <form id="getAssessmentResult" method="POST" action="">
+                                                        @csrf
+                                                        <div class="form-group">
+                                                            <label>Academic Session</label>
+                                                            <select class="form-control select2" name="academicSession"
+                                                                style="width: 100%;">
+                                                                @foreach ($academicSessions as $academicSession)
+                                                                <option @if (old('academicSession')==$academicSession )
+                                                                    SELECTED @endif>
+                                                                    {{ $academicSession->name }}
+                                                                </option>
+                                                                @endforeach
+                                                            </select>
+                                                            @error('academicSession')
+                                                            <div class="text-danger">{{ $message }}</div>
+                                                            @enderror
+
                                                         </div>
-                                                        <div class="form-group row">
-                                                            <label for="inputEmail"
-                                                                class="col-sm-2 col-form-label">Email</label>
-                                                            <div class="col-sm-10">
-                                                                <input type="email" class="form-control" id="inputEmail"
-                                                                    placeholder="Email">
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group row">
-                                                            <label for="inputName2"
-                                                                class="col-sm-2 col-form-label">Name</label>
-                                                            <div class="col-sm-10">
-                                                                <input type="text" class="form-control" id="inputName2"
-                                                                    placeholder="Name">
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group row">
-                                                            <label for="inputExperience"
-                                                                class="col-sm-2 col-form-label">Experience</label>
-                                                            <div class="col-sm-10">
-                                                                <textarea class="form-control" id="inputExperience"
-                                                                    placeholder="Experience"></textarea>
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group row">
-                                                            <label for="inputSkills"
-                                                                class="col-sm-2 col-form-label">Skills</label>
-                                                            <div class="col-sm-10">
-                                                                <input type="text" class="form-control" id="inputSkills"
-                                                                    placeholder="Skills">
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group row">
-                                                            <div class="offset-sm-2 col-sm-10">
-                                                                <div class="checkbox">
-                                                                    <label>
-                                                                        <input type="checkbox"> I agree to the <a
-                                                                            href="#">terms and conditions</a>
-                                                                    </label>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group row">
-                                                            <div class="offset-sm-2 col-sm-10">
-                                                                <button type="submit"
-                                                                    class="btn btn-danger">Submit</button>
-                                                            </div>
+                                                        <div class="form-group">
+                                                            <button type="submit"
+                                                                class="btn btn-primary">Submit</button>
                                                         </div>
                                                     </form>
                                                 </div>
@@ -397,11 +362,10 @@
         <script>
             function showEditModal(data) {
                 let editStudentUrl = '/edit/student/' + data.admission_no
-                let editGuardianUrl =  '/edit/guardian/' + data.guardian.phone
+                let editGuardianUrl = '/edit/guardian/' + data.guardian.phone
                 $('#editStudentButton').attr("href", editStudentUrl)
                 $('#editGuardianButton').attr("href", editGuardianUrl)
                 $('#editModal').modal('show')
-
             }
 
             function showViewModal(data) {
@@ -424,6 +388,10 @@
                 $('#viewModal #gEmail').html(data.guardian.email)
                 $('#viewModal #gPhone').html(data.guardian.phone)
                 $('#viewModal #gAddress').html(data.guardian.address)
+
+                // fill results form
+                let assessmentResultUrl = '/results/student/' + data.admission_no
+                $('#viewModal #getAssessmentResult').attr("action", assessmentResultUrl)
 
                 //show modal
                 $('#viewModal').modal('show')
