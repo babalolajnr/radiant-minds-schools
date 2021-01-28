32116c9d9472500b7a9939ac74c9c3aa4ff5c0c5
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-01-28 22:20:48 +0100

    added run method

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Student;
 use Illuminate\Database\Seeder;
 
 class StudentSeeder extends Seeder
@@ -13,6 +14,6 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Student::factory()->times(10)->create();
     }
 }
