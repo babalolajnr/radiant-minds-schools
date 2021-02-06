4e24dfb6420c447ddf7d1d578a9bd9ca7003f429
babalolajnr <babalolajnr@gmail.com>
2021-02-06 14:56:23 +0100

    commented out result tests

--- tests/Feature/ResultTest.php
+++ tests/Feature/ResultTest.php
@@ -14,19 +14,19 @@ class ResultTest extends TestCase
 {
     use RefreshDatabase;
     
-    public function test_user_can_store_assessment()
-    {
-        $this->withoutExceptionHandling();
-        $user = User::factory()->create();
-        $student = Student::factory()->create();
-        $subject = Subject::factory()->create();
-        $assessment =  Assessment::factory()->create();
+    // public function test_user_can_store_assessment()
+    // {
+    //     $this->withoutExceptionHandling();
+    //     $user = User::factory()->create();
+    //     $student = Student::factory()->create();
+    //     $subject = Subject::factory()->create();
+    //     $assessment =  Assessment::factory()->create();
 
-        $response = $this->actingAs($user)->post('store/assessment-result/' . $student->id . '/' . $subject->id, [
-            'mark' => mt_rand(0, $assessment->assessmentType->max_score), 
-            'assessment' => $assessment->name
-        ]);
+    //     $response = $this->actingAs($user)->post('store/assessment-result/' . $student->id . '/' . $subject->id, [
+    //         'mark' => mt_rand(0, $assessment->assessmentType->max_score), 
+    //         'assessment' => $assessment->name
+    //     ]);
 
-        $response->assertStatus(200);
-    }
+    //     $response->assertStatus(200);
+    // }
 }
