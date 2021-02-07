f796ed5ea1f370ee326c7a85d4a05c9f68595bd2
babalolajnr <babalolajnr@gmail.com>
2021-02-07 17:31:35 +0100

    reduced academic sessions

--- database/seeders/AcademicSessionSeeder.php
+++ database/seeders/AcademicSessionSeeder.php
@@ -18,13 +18,6 @@ class AcademicSessionSeeder extends Seeder
             '2010/2011',
             '2011/2012',
             '2012/2013',
-            '2013/2014',
-            '2014/2015',
-            '2015/2016',
-            '2016/2017',
-            '2017/2018',
-            '2018/2019',
-            '2019/2020',
         ];
 
         foreach ($academicSessions as $academicSession) {
