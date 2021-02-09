a6b1fb893e4483d9b255303333975ab1a1abfde8
babalolajnr <babalolajnr@gmail.com>
2021-02-09 16:06:34 +0100

    added test_user_can_store_results

--- tests/Feature/ResultTest.php
+++ tests/Feature/ResultTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\AcademicSession;
 use App\Models\Assessment;
 use App\Models\Student;
 use App\Models\Subject;
+use App\Models\Term;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -13,20 +15,22 @@ use Tests\TestCase;
 class ResultTest extends TestCase
 {
     use RefreshDatabase;
-    
-    // public function test_user_can_store_assessment()
-    // {
-    //     $this->withoutExceptionHandling();
-    //     $user = User::factory()->create();
-    //     $student = Student::factory()->create();
-    //     $subject = Subject::factory()->create();
-    //     $assessment =  Assessment::factory()->create();
 
-    //     $response = $this->actingAs($user)->post('store/assessment-result/' . $student->id . '/' . $subject->id, [
-    //         'mark' => mt_rand(0, $assessment->assessmentType->max_score), 
-    //         'assessment' => $assessment->name
-    //     ]);
+    public function test_user_can_store_results() {
+        $this->withoutExceptionHandling();
+        $user = User::factory()->create();
+        $student = Student::factory()->create();
+        $subject = Subject::factory()->create();
+        $academicSession = AcademicSession::factory()->create();
+        $term = Term::factory()->create();
 
-    //     $response->assertStatus(200);
-    // }
+        $response = $this->actingAs($user)->post('/store/result/'.$student->id.'/'.$subject->id, [
+            'ca' => mt_rand(0, 40),
+            'exam' => mt_rand(0, 60),
+            'academicSession' => $academicSession->name,
+            'term' => $term->name,
+        ]);
+
+        $response->assertStatus(200);
+    }
 }
