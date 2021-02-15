88fed4cec8a10afe56ff4b2ae4019f43a9afe38d
babalolajnr <babalolajnr@gmail.com>
2021-02-15 16:16:38 +0100

    created user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'first_name' => 'Abdulqudduus',
+            'last_name' => 'Babalola',
+            'user_type' => 'master',
+            'email' => 'babalolajnr@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('11111111'), // 8 ones
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
