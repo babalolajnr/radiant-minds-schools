be3b30fb300adc1c23fa0e1c27f9a35e6b363a2b
babalolajnr <babalolajnr@gmail.com>
2021-02-18 18:55:20 +0100

    added constraint to name field

--- database/migrations/2021_01_06_155413_create_subjects_table.php
+++ database/migrations/2021_01_06_155413_create_subjects_table.php
@@ -15,7 +15,7 @@ class CreateSubjectsTable extends Migration
     {
         Schema::create('subjects', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
