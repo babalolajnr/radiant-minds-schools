a0375c557b763d2ab25b1f9faf973eceeb8dc30f
babalolajnr <babalolajnr@gmail.com>
2021-02-18 18:40:34 +0100

    refactored run method

--- database/seeders/ClassroomSeeder.php
+++ database/seeders/ClassroomSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Classroom;
 use App\Models\Subject;
-use App\Models\Teacher;
+use Database\Factories\ClassroomFactory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,36 +17,7 @@ class ClassroomSeeder extends Seeder
      */
     public function run()
     {
-        $classes = [
-            'Pre nursery',
-            'Nursery 1',
-            'Nursery 2',
-            'Reception',
-            'Grade 1',
-            'Grade 2',
-            'Grade 3',
-            'Grade 4',
-            'Grade 5',
-        ];
-
-        $subject = Subject::inRandomOrder()->first();
-
-        if (!$subject) {
-            Artisan::call('db:seed', ['--class' => 'SubjectSeeder']);
-            $subject = Subject::inRandomOrder()->limit(5)->get()->pluck('id');
-        } else {
-            $subject = Subject::inRandomOrder()->limit(5)->get()->pluck('id');
-        }
-
-        foreach ($classes as $class) {
-            $classroom = Classroom::create(
-                [
-                    'name' => $class,
-                    'teacher_id' => Teacher::factory()->create(['status' => 'active'])->id
-                ]
-            );
-
-            $classroom->subjects()->sync($subject);
-        }
+        $classes = ClassroomFactory::$classes;
+        Classroom::factory()->times(count($classes))->create();
     }
 }
