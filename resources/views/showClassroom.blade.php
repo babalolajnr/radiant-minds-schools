d0e72dfff9d7bdc3f1dc9f7810a283c60e803f99
babalolajnr <babalolajnr@gmail.com>
2021-02-19 17:31:10 +0100

    minor change

--- resources/views/showClassroom.blade.php
+++ resources/views/showClassroom.blade.php
@@ -565,15 +565,7 @@
                     "autoWidth": false,
                     "buttons": ["copy", "csv", "excel", "pdf", "print"]
                 }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-                // $('#example2').DataTable({
-                //     "paging": true,
-                //     "lengthChange": false,
-                //     "searching": false,
-                //     "ordering": true,
-                //     "info": true,
-                //     "autoWidth": false,
-                //     "responsive": true,
-                // });
+               
             });
 
         </script>
