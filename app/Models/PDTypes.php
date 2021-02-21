211764269bf7d8f2b0a709729958b9e78abdd16b
babalolajnr <babalolajnr@gmail.com>
2021-02-21 15:21:18 +0100

    created PDTypes

---
+++ app/Models/PDTypes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PDTypes extends Model
+{
+    use HasFactory;
+}
---
+++ database/factories/PDTypesFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\PDTypes;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PDTypesFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = PDTypes::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
