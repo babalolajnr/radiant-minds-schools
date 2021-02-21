b0249c3a4fa13b7d82f08b3047e1e3aec79efa2c
babalolajnr <babalolajnr@gmail.com>
2021-02-21 15:20:10 +0100

    added relationsgips and fillable array

--- app/Models/PD.php
+++ app/Models/PD.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class PD extends Model
 {
     use HasFactory;
+    protected $table = 'p_d_s';
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function student()
+    {
+        $this->belongsTo(Student::class);
+    }
+
+    public function academicSession()
+    {
+        $this->belongsTo(AcademicSession::class);
+    }
+
+    public function term()
+    {
+        $this->belongsTo(Term::class);
+    }
 }
