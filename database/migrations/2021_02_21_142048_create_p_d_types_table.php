3a3c4aca8845184f77961ed3019dc8306d534afc
babalolajnr <babalolajnr@gmail.com>
2021-02-22 17:22:29 +0100

    added slug field

--- database/migrations/2021_02_21_142048_create_p_d_types_table.php
+++ database/migrations/2021_02_21_142048_create_p_d_types_table.php
@@ -15,7 +15,8 @@ class CreatePDTypesTable extends Migration
     {
         Schema::create('p_d_types', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
