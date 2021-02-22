3c678888e4efbc730c1e420d7c04881cd7bf574b
babalolajnr <babalolajnr@gmail.com>
2021-02-22 17:20:09 +0100

    added create method

--- app/Http/Controllers/PDController.php
+++ app/Http/Controllers/PDController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PDTypes;
+use App\Models\Student;
+use App\Models\Term;
 use Illuminate\Http\Request;
 
 class PDController extends Controller
 {
-    //
+    public function create($id)
+    {
+        $student = Student::findOrFail($id);
+        $pdTypes = PDTypes::all();
+        $terms = Term::all();
+        return view('createPD', compact('pdTypes', 'student', 'terms'));
+    }
+
+    public function store($id, Request $request, $termId)
+    {
+        
+    }
 }
