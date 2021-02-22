fdbff0a359dc91bf617cc7b8f04f9c7c69ae3bbf
babalolajnr <babalolajnr@gmail.com>
2021-02-22 07:02:24 +0100

    added comments

--- app/Models/PD.php
+++ app/Models/PD.php
@@ -5,29 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * PD stands for Pyschomotor domain
+ */
 class PD extends Model
 {
-    use HasFactory;
-    protected $table = 'p_d_s';
-    protected $guarded = ['id', 'created_at', 'updated_at'];
+   use HasFactory;
+   
+   protected $table = 'p_d_s';
+   protected $guarded = ['id', 'created_at', 'updated_at'];
 
-    public function student()
-    {
-       return $this->belongsTo(Student::class);
-    }
+   public function student()
+   {
+      return $this->belongsTo(Student::class);
+   }
 
-    public function academicSession()
-    {
-       return $this->belongsTo(AcademicSession::class);
-    }
+   public function academicSession()
+   {
+      return $this->belongsTo(AcademicSession::class);
+   }
 
-    public function term()
-    {
-       return $this->belongsTo(Term::class);
-    }
+   public function term()
+   {
+      return $this->belongsTo(Term::class);
+   }
 
-    public function pdType()
-    {
-        return $this->belongsTo(PDTypes::class);
-    }
+   public function pdType()
+   {
+      return $this->belongsTo(PDTypes::class);
+   }
 }
