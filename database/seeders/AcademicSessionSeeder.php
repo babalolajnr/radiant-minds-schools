00ec4c186b05a71ea4a370d7eb6aba26c8047525
babalolajnr <babalolajnr@gmail.com>
2021-02-22 21:43:24 +0100

    added seed currnt academic session functionality

--- database/seeders/AcademicSessionSeeder.php
+++ database/seeders/AcademicSessionSeeder.php
@@ -18,5 +18,11 @@ class AcademicSessionSeeder extends Seeder
 
         $academicSessions = AcademicSessionFactory::$academicSessions;
         AcademicSession::factory()->times(count($academicSessions))->create();
+
+        //seed current academic session
+        $academicSession = AcademicSession::inRandomOrder()->first();
+        AcademicSession::where('current_session', 1)->update(['current_session' => null]);
+        $academicSession->current_session = 1;
+        $academicSession->save();
     }
 }
