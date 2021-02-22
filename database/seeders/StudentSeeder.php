84d0f05c11390748e7f95733fa064fbc15865ff5
babalolajnr <babalolajnr@gmail.com>
2021-02-22 17:24:01 +0100

    changed times() parameter

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -14,6 +14,6 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        Student::factory()->times(10)->create();
+        Student::factory()->times(50)->create();
     }
 }
