a6ae30b272a27037135d31466b473e82af9137d2
babalolajnr <babalolajnr@gmail.com>
2021-02-28 10:41:04 +0100

    added dashboard link to logo and added user icon to the users name display

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,19 +1,16 @@
     <!-- Well begun is half done. - Aristotle -->
     <aside class="main-sidebar main-sidebar-custom sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="../../index3.html" class="brand-link">
-            <img src="{{ asset('TAssets')}}dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
-                class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <a href="/dashboard" class="brand-link">
+            <span class="brand-text font-weight-bold">Radiant Minds Schools</span>
         </a>
 
         <!-- Sidebar -->
         <div class="sidebar">
             <!-- Sidebar user (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div class="image">
-                    <img src="{{ asset('TAssets')}}dist/img/user2-160x160.jpg" class="img-circle elevation-2"
-                        alt="User Image">
+                <div class="image"> 
+                    <i class="fas fa-user text-white"></i>
                 </div>
                 <div class="info">
                     <a href="#" class="d-block">{{ Auth::user()->first_name }}</a>
@@ -56,7 +53,7 @@
                             </li>
                             <li class="nav-item">
                                 <a href="/classrooms" class="nav-link">
-                                    <i class="nav-icon fa fa-list-alt"  ></i>
+                                    <i class="nav-icon fa fa-list-alt"></i>
                                     <p>Classrooms</p>
                                 </a>
                             </li>
@@ -84,7 +81,7 @@
                                     <p>New Student</p>
                                 </a>
                             </li>
-                           
+
                         </ul>
                     </li>
 
