2927c0cb8aecd9581492f36c73895d0917f287c7
babalolajnr <babalolajnr@gmail.com>
2021-02-28 19:17:51 +0100

    added seed logic

--- database/seeders/PDSeeder.php
+++ database/seeders/PDSeeder.php
@@ -2,7 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\AcademicSession;
+use App\Models\PD;
+use App\Models\PDType;
+use App\Models\Student;
+use App\Models\Term;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class PDSeeder extends Seeder
 {
@@ -13,6 +19,87 @@ class PDSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $allRecords = $this->allRecords();
+
+        //generate 2000 random results
+        for ($i = 0; $i < 1500; $i++) {
+            $values = $this->getRandomValues($allRecords);
+
+            //get record where subject_id,term_id,student_id and academic_session_id exists 
+            $record = PD::where('p_d_type_id', $values['pdType']->id)
+                ->where('student_id', $values['student']->id)
+                ->where('term_id', $values['term']->id)
+                ->where('academic_session_id', $values['academicSession']->id);
+
+            while ($record->exists()) {
+                $values = $this->getRandomValues($allRecords);
+
+                $record = PD::where('p_d_type_id', $values['pdType']->id)
+                    ->where('student_id', $values['student']->id)
+                    ->where('term_id', $values['term']->id)
+                    ->where('academic_session_id', $values['academicSession']->id);
+            }
+
+            $value = mt_rand(1,5);
+            PD::create([
+                'term_id' => $values['term']->id,
+                'academic_session_id' => $values['academicSession']->id,
+                'student_id' => $values['student']->id,
+                'value' => $value,
+                'p_d_type_id' => $values['pdType']->id
+            ]);
+        }
+    }
+
+    private function allRecords()
+    {
+        $term = Term::first();
+        $academicSession = AcademicSession::first();
+        $student = Student::first();
+        $pdType = PDType::first();
+
+        //if any of the required values are empty seed their tables
+        if (is_null($term)) {
+            Artisan::call('db:seed', ['--class' => 'TermSeeder']);
+        }
+
+        if (is_null($academicSession)) {
+            Artisan::call('db:seed', ['--class' => 'AcademicSessionSeeder']);
+        }
+
+        if (is_null($student)) {
+            Artisan::call('db:seed', ['--class' => 'StudentSeeder']);
+        }
+
+        if (is_null($pdType)) {
+            Artisan::call('db:seed', ['--class' => 'PDTypeSeeder']);
+        }
+
+        $terms = Term::all();
+        $academicSessions = AcademicSession::all();
+        $students = Student::all();
+        $pdTypes = PDType::all();
+
+        return [
+            'terms' => $terms,
+            'academicSessions' => $academicSessions,
+            'students' => $students,
+            'pdTypes' => $pdTypes
+        ];
+    }
+
+    private function getRandomValues($allRecords)
+    {
+        $student = $allRecords['students']->random();
+        $term = $allRecords['terms']->random();
+        $academicSession = $allRecords['academicSessions']->random();
+        $pdType = $allRecords['pdTypes']->random();
+
+        return [
+            'student' => $student,
+            'term' => $term,
+            'academicSession' => $academicSession,
+            'pdType' => $pdType
+        ];
     }
 }
