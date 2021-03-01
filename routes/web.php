4caf5b06aba62f3fc658ddf659a7cc92862c8e97
babalolajnr <babalolajnr@gmail.com>
2021-03-01 14:31:59 +0100

    added pdType store route

--- routes/web.php
+++ routes/web.php
@@ -113,6 +113,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     //Pychomotor domain type routes
     Route::get('/pdTypes', [PDTypesController::class, 'index']);
+    Route::post('/store/pdType', [PDTypesController::class, 'store']);
 
 });
 
