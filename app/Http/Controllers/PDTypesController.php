1b7f138131286a71348f0d3cbae71ea686f2e0a8
babalolajnr <babalolajnr@gmail.com>
2021-03-01 13:51:06 +0100

    added index method

--- app/Http/Controllers/PDTypesController.php
+++ app/Http/Controllers/PDTypesController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PDType;
 use Illuminate\Http\Request;
 
 class PDTypesController extends Controller
 {
-    //
+    public function index()
+    {
+        $pdTypes = PDType::all();
+        return view('pdTypes', compact('pdTypes'));
+    }
 }
