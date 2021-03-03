5cccdf6432591543b242844d73f480d87db97528
babalolajnr <babalolajnr@gmail.com>
2021-03-03 09:04:32 +0100

    added confirmation modal for promote and delete actions

--- resources/views/studentSettings.blade.php
+++ resources/views/studentSettings.blade.php
@@ -91,20 +91,17 @@
                                         <label for="class" class="col-sm-2 col-form-label">Class</label>
                                         <div class="col-sm-10">
                                             <div class="btn-group">
-                                                <form action="/promote/student/{{ $student->id }}" method="post">
-                                                    @csrf
-                                                    @method('PATCH')
-                                                    <button type="submit" class="btn btn-success btn-flat">
-                                                        Promote
-                                                    </button>
-                                                </form>
-                                                <form action="/demote/student/{{ $student->id }}" method="post">
-                                                    @csrf
-                                                    @method('PATCH')
-                                                    <button type="submit" class="btn btn-danger btn-flat">
-                                                        Demote
-                                                    </button>
-                                                </form>
+                                                <button type="button"
+                                                    onclick="changeClassConfirmationModal({{ $student }}, 'promote')"
+                                                    class="btn btn-success btn-flat">
+                                                    Promote
+                                                </button>
+
+                                                <button type="button"
+                                                    onclick="changeClassConfirmationModal({{ $student }}, 'demote')"
+                                                    class="btn btn-danger btn-flat">
+                                                    Demote
+                                                </button>
                                             </div>
                                         </div>
                                     </div>
@@ -158,6 +155,32 @@
         <!-- /.modal-dialog -->
     </div>
 
+    {{-- change class confirmation modal --}}
+    <div class="modal fade" id="changeClassConfirmationModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Confirmation</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to <span id="confirmationModalStudentName" class="font-bold"></span>?
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <form action="" id="changeClassForm" method="post">
+                        @csrf
+                        @method('PATCH')
+                        <button type="submit" class="btn btn-danger">Yes</button>
+                    </form>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
     <x-slot name="scripts">
         <!-- Toastr -->
         <script src="{{ asset('TAssets/plugins/toastr/toastr.min.js') }}"></script>
@@ -185,6 +208,23 @@
         </script>
         <!-- AdminLTE App -->
         <script>
+            function changeClassConfirmationModal(data, action) {
+
+                const studentName = data.first_name + ' ' + data.last_name
+
+                if (action == 'promote') {
+                    let promoteStudentUrl = '/promote/student/' + data.id
+                    $('#changeClassForm').attr("action", promoteStudentUrl)
+                    $('#confirmationModalStudentName').html(action + ' ' + studentName)
+                    $('#changeClassConfirmationModal').modal('show')
+                } else if (action == 'demote') {
+                    let demoteStudentUrl = '/demote/student/' + data.id
+                    $('#changeClassForm').attr("action", demoteStudentUrl)
+                    $('#confirmationModalStudentName').html(action + ' ' + studentName)
+                    $('#changeClassConfirmationModal').modal('show')
+                }
+            }
+            
             //launch toastr 
             $(function() {
                 let Success = document.getElementById('success')
