1d0c8ebf5dd0183b436773255f74d52bb565b6de
babalolajnr <babalolajnr@gmail.com>
2021-03-04 12:43:33 +0100

    fixed new student link

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -72,7 +72,7 @@
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="create/student" class="nav-link">
+                                <a href="/create/student" class="nav-link">
                                     <i class="nav-icon fas fa-plus"></i>
                                     <p>New Student</p>
                                 </a>
