58b8a65c0ff1e9b6d0102285b8487279e9248190
babalolajnr <babalolajnr@gmail.com>
2021-03-11 15:42:51 +0100

    added slug fielf to fillable array

--- app/Models/Term.php
+++ app/Models/Term.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Term extends Model
 {
     use HasFactory;
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'slug'];
 
     public function Results()
     {
