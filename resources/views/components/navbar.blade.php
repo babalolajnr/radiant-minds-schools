23d77f4b8e88c4072853278ffea4f78bf6b900cf
babalolajnr <babalolajnr@gmail.com>
2021-03-13 06:46:37 +0100

    removed control side bar link

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -6,21 +6,15 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="/dashboard" class="nav-link">Home</a>
+            <a href="{{ route('dashboard') }}" class="nav-link">Home</a>
         </li>
     </ul>
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-
         <li class="nav-item">
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                 <i class="fas fa-expand-arrows-alt"></i>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-                <i class="fas fa-th-large"></i>
             </a>
         </li>
     </ul>
