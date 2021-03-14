e89165215ba2411d463ef8a5709c0abcccfae61a
babalolajnr <babalolajnr@gmail.com>
2021-03-14 19:50:52 +0100

    added deactivated user view

---
+++ resources/views/deactivated.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        @import url("https://fonts.googleapis.com/css?family=Share+Tech+Mono|Montserrat:700");
+
+        * {
+            margin: 0;
+            padding: 0;
+            border: 0;
+            font-size: 100%;
+            font: inherit;
+            vertical-align: baseline;
+            box-sizing: border-box;
+            color: inherit;
+        }
+
+        body {
+            background-image: linear-gradient(120deg, #4f0088 0%, #000000 100%);
+            height: 100vh;
+        }
+
+        h1 {
+            font-size: 45vw;
+            text-align: center;
+            position: fixed;
+            width: 100vw;
+            z-index: 1;
+            color: #ffffff26;
+            text-shadow: 0 0 50px rgba(0, 0, 0, 0.07);
+            top: 50%;
+            transform: translateY(-50%);
+            font-family: "Montserrat", monospace;
+        }
+
+        div {
+            background: rgba(0, 0, 0, 0);
+            width: 70vw;
+            position: relative;
+            top: 50%;
+            transform: translateY(-50%);
+            margin: 0 auto;
+            padding: 30px 30px 10px;
+            box-shadow: 0 0 150px -20px rgba(0, 0, 0, 0.5);
+            z-index: 3;
+        }
+
+        P {
+            font-family: "Share Tech Mono", monospace;
+            color: #f5f5f5;
+            margin: 0 0 20px;
+            font-size: 17px;
+            line-height: 1.2;
+        }
+
+        span {
+            color: #f0c674;
+        }
+
+        i {
+            color: #8abeb7;
+        }
+
+        div a {
+            text-decoration: none;
+        }
+
+        b {
+            color: #81a2be;
+        }
+
+        @keyframes slide {
+            from {
+                right: -100px;
+                transform: rotate(360deg);
+                opacity: 0;
+            }
+
+            to {
+                right: 15px;
+                transform: rotate(0deg);
+                opacity: 1;
+            }
+        }
+
+    </style>
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>403</h1>
+    <div>
+        <p>> <span>ERROR CODE</span>: "<i>HTTP 403 Forbidden</i>"</p>
+        <p>> <span>ERROR DESCRIPTION</span>: "<i>Access Denied. You Do Not Have The Permission To Access This Page On
+                This Server</i>"</p>
+        <p>> <span>ERROR POSSIBLY CAUSED BY</span>: [<b>Your account might have been deactivated by the
+                Administrator</b>...]</p>
+        <p>> <span>POSSIBLE SOLUTION</span>: [<b>Contact the administrator to rectify the issue </b>]
+        </p>
+        <p>> <span>HAVE A NICE DAY :-)</span></p>
+    </div>
+
+    <!-- <a class="avatar" href="https://codepen.io/leenalavanya/">Designed by Leenalavanya</a> -->
+    <script>
+        var str = document.getElementsByTagName('div')[0].innerHTML.toString();
+        var i = 0;
+        document.getElementsByTagName('div')[0].innerHTML = "";
+
+        setTimeout(function() {
+            var se = setInterval(function() {
+                i++;
+                document.getElementsByTagName('div')[0].innerHTML = str.slice(0, i) + "|";
+                if (i == str.length) {
+                    clearInterval(se);
+                    document.getElementsByTagName('div')[0].innerHTML = str;
+                }
+            }, 10);
+        }, 0);
+
+    </script>
+</body>
+
+</html>
