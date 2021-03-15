9058bdb008029ee13b6f91bad9c2debce19bb5d7
babalolajnr <babalolajnr@gmail.com>
2021-03-15 12:13:13 +0100

    added isVerified method

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable
     {
         return $this->status == 'active';
     }
+
+    public function isVerified()
+    {
+        return $this->is_verified == true;
+    }
 }
