43954da3ffb1284878edfe12704417515e16d2dd
babalolajnr <babalolajnr@gmail.com>
2021-03-16 13:00:24 +0100

    added factory logic

--- database/factories/ADTypeFactory.php
+++ database/factories/ADTypeFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\ADType;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ADTypeFactory extends Factory
@@ -21,8 +22,22 @@ class ADTypeFactory extends Factory
      */
     public function definition()
     {
+        $adType = $this->faker->unique()->randomElement(self::$adTypes);
+        $slug = Str::of($adType)->slug('-');
         return [
-            //
+            'name' => $adType,
+            'slug' => $slug
         ];
     }
+
+    public static $adTypes = [
+        'Punctuality',
+        'Attendance',
+        'Neatness',
+        'Politeness',
+        'Attentiveness',
+        'Self control/calmness',
+        'Obedience',
+        'Relationship with others',
+    ];
 }
