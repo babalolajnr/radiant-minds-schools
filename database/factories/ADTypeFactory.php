c9327f775794df8884336a1e2c7d7a1d7d875160
babalolajnr <babalolajnr@gmail.com>
2021-03-16 08:01:07 +0100

    created ad and adtype with controllers, migrations. seeders and factories

---
+++ database/factories/ADTypeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ADType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ADTypeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ADType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
