5cae96185f92648172509b167c622dc90a6aeac1
babalolajnr <babalolajnr@gmail.com>
2021-03-16 14:30:53 +0100

    removed unused assets

--- resources/views/createPD.blade.php
+++ resources/views/createPD.blade.php
@@ -2,12 +2,6 @@
     <x-slot name="styles">
         <!-- Toastr -->
         <link rel="stylesheet" href="{{ asset('TAssets/plugins/toastr/toastr.min.css') }}">
-        <!-- DataTables -->
-        <link rel="stylesheet" href="{{ asset('TAssets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-        <link rel="stylesheet"
-            href="{{ asset('TAssets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
-        <link rel="stylesheet"
-            href="{{ asset('TAssets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
         <link rel="stylesheet" href="{{ asset('css/range-slider.min.css') }}">
     </x-slot>
 
@@ -93,28 +87,6 @@
     <x-slot name="scripts">
         <!-- Toastr -->
         <script src="{{ asset('TAssets/plugins/toastr/toastr.min.js') }}"></script>
-        <!-- DataTables  & Plugins -->
-        <script src="{{ asset('TAssets/plugins/datatables/jquery.dataTables.min.js') }}">
-        </script>
-        <script src="{{ asset('TAssets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}">
-        </script>
-        <script src="{{ asset('TAssets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}">
-        </script>
-        <script src="{{ asset('TAssets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}">
-        </script>
-        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}">
-        </script>
-        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}">
-        </script>
-        <script src="{{ asset('TAssets/plugins/jszip/jszip.min.js') }}"></script>
-        <script src="{{ asset('TAssets/plugins/pdfmake/pdfmake.min.js') }}"></script>
-        <script src="{{ asset('TAssets/plugins/pdfmake/vfs_fonts.js') }}"></script>
-        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/buttons.html5.min.js') }}">
-        </script>
-        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/buttons.print.min.js') }}">
-        </script>
-        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/buttons.colVis.min.js') }}">
-        </script>
         <script src="{{ asset('js/range-slider.min.js') }}">
         </script>
         <!-- AdminLTE App -->
