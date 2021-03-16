6cd4686a7dd3e671e98f75b5cde1a230a4c1cd3b
babalolajnr <babalolajnr@gmail.com>
2021-03-16 12:42:39 +0100

    added relationships and guarded fields

--- app/Models/AD.php
+++ app/Models/AD.php
@@ -5,7 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * AD stands for Affective Domain
+ */
 class AD extends Model
 {
     use HasFactory;
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function academicSession()
+    {
+        return $this->belongsTo(AcademicSession::class);
+    }
+
+    public function term()
+    {
+        return $this->belongsTo(Term::class);
+    }
+
+    public function adType()
+    {
+        return $this->belongsTo(ADType::class);
+    }
 }
