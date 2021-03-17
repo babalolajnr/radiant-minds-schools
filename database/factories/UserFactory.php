d56cdad20f72401ff6ba77c6561134e5b4035bc5
babalolajnr <babalolajnr@gmail.com>
2021-03-17 15:40:44 +0100

    added is_verified and status fields

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -31,6 +31,8 @@ class UserFactory extends Factory
             'user_type' => $this->faker->randomElement(['admin', 'master']),
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
+            'is_verified' => true,
+            'status' => 'active',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
