f092d80f3e3c0b4b6e4180aba49517daaeb13171
babalolajnr <babalolajnr@gmail.com>
2021-03-18 12:30:33 +0100

    refactored subject tests

--- tests/Feature/SubjectTest.php
+++ tests/Feature/SubjectTest.php
@@ -17,7 +17,7 @@ class SubjectTest extends TestCase
     public function test_subject_index_method()
     {
         $user = User::factory()->create();
-        $response = $this->actingAs($user)->get('/terms');
+        $response = $this->actingAs($user)->get(route('subject.index'));
         $response->assertStatus(200);
     }
 
@@ -26,44 +26,36 @@ class SubjectTest extends TestCase
         $user = User::factory()->create();
         $name = $this->faker->word;
         $slug = Str::of($name)->slug('-');
-        $response = $this->actingAs($user)->post('/store/subject', [
+        $response = $this->actingAs($user)->post(route('subject.store'), [
             'name' => $name,
             'slug' => $slug
         ]);
-        $response->assertStatus(200);
+        $response->assertStatus(302)->assertSessionHas('success');
     }
 
     public function test_subject_edit_method()
     {
         $user = User::factory()->create();
-        $subject = Subject::factory()->create()->id;
-        $response = $this->actingAs($user)->get('/edit/subject/' . $subject);
+        $subject = Subject::factory()->create();
+        $response = $this->actingAs($user)->get(route('subject.edit', ['subject' => $subject]));
         $response->assertStatus(200);
     }
 
     public function test_subject_update_method()
     {
         $user = User::factory()->create();
-        $subject = Subject::factory()->create()->id;
-        $response = $this->actingAs($user)->patch('/update/subject/' . $subject, [
+        $subject = Subject::factory()->create();
+        $response = $this->actingAs($user)->patch(route('subject.update', ['subject' => $subject]), [
             'name' => $this->faker->word
         ]);
-        $response->assertStatus(200);
+        $response->assertStatus(302)->assertSessionHas('success');
     }
 
-    public function test_master_can_delete_a_subject()
+    public function test_user_can_delete_a_subject()
     {
-        $user = User::factory()->create(['user_type' => 'master']);
-        $subject = Subject::factory()->create()->id;
-        $response = $this->actingAs($user)->delete('/delete/subject/' . $subject);
-        $response->assertStatus(200);
-    }
-
-    public function test_admin_cannot_delete_subject()
-    {
-        $user = User::factory()->create(['user_type' => 'admin']);
-        $subject = Subject::factory()->create()->id;
-        $response = $this->actingAs($user)->delete('/delete/subject/' . $subject);
-        $response->assertStatus(403);
+        $user = User::factory()->create();
+        $subject = Subject::factory()->create();
+        $response = $this->actingAs($user)->delete(route('subject.destroy', ['subject' => $subject]));
+        $response->assertStatus(302)->assertSessionHas('success');
     }
 }
