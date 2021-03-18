868b24820f13c9edb2aaec61fe682157ee4e619e
babalolajnr <babalolajnr@gmail.com>
2021-03-18 12:30:14 +0100

    removed usnused routes from teacher routes

--- routes/web.php
+++ routes/web.php
@@ -54,9 +54,7 @@ Route::middleware(['auth', 'verified', 'isActiveAndVerified'])->group(function (
         Route::patch('/suspend/{teacher}', [TeacherController::class, 'suspend'])->name('suspend');
         Route::patch('/activate/{teacher}', [TeacherController::class, 'activate'])->name('activate');
         Route::patch('/deactivate/{teacher}', [TeacherController::class, 'deactivate'])->name('deactivate');
-        // Route::patch('/restore/{id}', [TeacherController::class, 'restore'])->name('restore');
         Route::delete('/delete/{teacher}', [TeacherController::class, 'destroy'])->name('destroy');
-        // Route::delete('/force-delete/{id}', [TeacherController::class, 'forceDelete'])->name('force.delete');
     });
 
 
