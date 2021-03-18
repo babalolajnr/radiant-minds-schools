cf717b256e57e325bf49dd5e2e84d7b21c20dd06
babalolajnr <babalolajnr@gmail.com>
2021-03-18 12:30:53 +0100

    refactored tests

--- tests/Feature/TeacherTest.php
+++ tests/Feature/TeacherTest.php
@@ -13,102 +13,79 @@ class TeacherTest extends TestCase
     use WithFaker;
     use RefreshDatabase;
 
-    public function test_admin_can_access_create_teacher_view()
+    public function test_user_can_access_create_teacher_view()
     {
-        $user = User::factory()->create(['user_type' => 'admin']);
-        $response = $this->actingAs($user)->get('/create/teacher');
+        $this->withoutExceptionHandling();
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get(route('teacher.create'));
         $response->assertStatus(200);
     }
 
-    public function test_admin_can_store_a_new_teacher()
+    public function test_user_can_store_a_new_teacher()
     {
-        $user = User::factory()->create(['user_type' => 'admin']);
-        $response = $this->actingAs($user)->post('/store/teacher', [
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->post(route('teacher.store'), [
             'first_name' => $this->faker->firstName,
             'last_name' => $this->faker->lastName,
             'email' => $this->faker->email,
             'phone' => $this->faker->e164PhoneNumber,
             'date_of_birth' => $this->faker->dateTimeThisCentury(),
         ]);
-        $response->assertStatus(200);
+        $response->assertStatus(302)->assertSessionHas('success');
     }
 
-    public function test_a_single_teacher_record_can_be_viewed()
+    public function test_a_single_teacher_can_be_viewed()
     {
         $user = User::factory()->create();
         $teacher = Teacher::factory()->create();
-        $response = $this->actingAs($user)->get('/view/teacher/' . $teacher->slug);
+        $response = $this->actingAs($user)->get(route('teacher.show', ['teacher' => $teacher]));
         $response->assertStatus(200);
     }
 
-    public function test_admin_can_access_edit_teacher_view()
+    public function test_user_can_update_teacher()
     {
-        $user = User::factory()->create(['user_type' => 'admin']);
+        $user = User::factory()->create();
         $teacher = Teacher::factory()->create();
-        $response = $this->actingAs($user)->get('/edit/teacher/' . $teacher->slug);
-        $response->assertStatus(200);
-    }
-
-    public function test_admin_can_update_teacher()
-    {
-        $user = User::factory()->create(['user_type' => 'admin']);
-        $teacher = Teacher::factory()->create();
-        $response = $this->actingAs($user)->patch('/update/teacher/' . $teacher->slug, [
+        $response = $this->actingAs($user)->patch(route('teacher.update', ['teacher' => $teacher]), [
             'first_name' => $this->faker->firstName,
             'last_name' => $this->faker->lastName,
             'email' => $this->faker->email,
             'phone' => $this->faker->e164PhoneNumber,
             'date_of_birth' => $this->faker->dateTimeThisCentury(),
         ]);
-        $response->assertStatus(200);
-
+        $response->assertStatus(302)->assertSessionHas('success');
     }
 
-    public function test_admin_cannot_delete_teacher()
+    public function test_user_can_delete_teacher()
     {
-        $user = User::factory()->create(['user_type' => 'admin']);
+        $user = User::factory()->create();
         $teacher = Teacher::factory()->create();
-        $response = $this->actingAs($user)->delete('/delete/teacher/' . $teacher->id);
-        $response->assertStatus(403);
+        $response = $this->actingAs($user)->delete(route('teacher.destroy', ['teacher' => $teacher]));
+        $response->assertStatus(302)->assertSessionHas('success');
     }
 
-    public function test_master_can_force_delete_teacher()
+    public function test_user_can_suspend_teacher()
     {
-        $user = User::factory()->create(['user_type' => 'master']);
-        $teacher = Teacher::factory()->create();
-        $response = $this->actingAs($user)->delete('/forceDelete/teacher/' . $teacher->id);
-        $response->assertStatus(200);
-    }
-
-    public function test_master_can_suspend_teacher()
-    {
-        $user = User::factory()->create(['user_type' => 'master']);
+        $user = User::factory()->create();
         $teacher = Teacher::factory()->create(['status' => 'active']);
-        $response = $this->actingAs($user)->patch('/suspend/teacher/' . $teacher->id);
-        $response->assertStatus(200);
+        $response = $this->actingAs($user)->patch(route('teacher.suspend', ['teacher' => $teacher]));
+        $response->assertStatus(302)->assertSessionHas('success');
     }
 
-    public function test_master_can_activate_teacher()
+    public function test_user_can_activate_teacher()
     {
-        $user = User::factory()->create(['user_type' => 'master']);
+        $user = User::factory()->create();
         $teacher = Teacher::factory()->create(['status' => 'suspended']);
-        $response = $this->actingAs($user)->patch('/activate/teacher/' . $teacher->id);
-        $response->assertStatus(200);
+        $response = $this->actingAs($user)->patch(route('teacher.activate', ['teacher' => $teacher]));
+        $response->assertStatus(302)->assertSessionHas('success');
     }
 
-    public function test_master_can_deactivate_teacher()
+    public function test_user_can_deactivate_teacher()
     {
-        $user = User::factory()->create(['user_type' => 'master']);
+        $this->withoutExceptionHandling();
+        $user = User::factory()->create();
         $teacher = Teacher::factory()->create(['status' => 'active']);
-        $response = $this->actingAs($user)->patch('/deactivate/teacher/' . $teacher->id);
-        $response->assertStatus(200);
-    }
-
-    public function test_master_can_restore_teacher()
-    {
-        $user = User::factory()->create(['user_type' => 'master']);
-        $teacher = Teacher::factory()->create(['deleted_at' => now()]);
-        $response = $this->actingAs($user)->patch('/restore/teacher/' . $teacher->id);
-        $response->assertStatus(200);
+        $response = $this->actingAs($user)->patch(route('teacher.deactivate', ['teacher' => $teacher]));
+        $response->assertStatus(302)->assertSessionHas('success');
     }
 }
