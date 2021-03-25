3510ae8649205bbf468fccedaf06d39c190db4b2
babalolajnr <babalolajnr@gmail.com>
2021-03-25 12:01:20 +0100

    added seeder classes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            AcademicSessionSeeder::class,
+            ADTypeSeeder::class,
+            ADSeeder::class,
+            ClassroomSeeder::class,
+            ClassroomSubjectSeeder::class,
+            GuardianSeeder::class,
+            PDSeeder::class,
+            PDTypeSeeder::class,
+            ResultSeeder::class,
+            StudentSeeder::class,
+            SubjectSeeder::class,
+            TeacherSeeder::class,
+            TermSeeder::class,
+        ]);
     }
 }
