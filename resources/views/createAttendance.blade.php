5cfbd2f917645e765a2e55bef0a4d668f001a51d
babalolajnr <babalolajnr@gmail.com>
2021-03-30 16:34:52 +0100

    created create attendance view

---
+++ resources/views/createAttendance.blade.php
@@ -0,0 +1,114 @@
+<x-app-layout>
+    <x-slot name="styles">
+        <!-- Toastr -->
+        <link rel="stylesheet" href="{{ asset('TAssets/plugins/toastr/toastr.min.css') }}">
+        <!-- Select2 -->
+        <link rel="stylesheet" href="{{ asset('TAssets/plugins/select2/css/select2.min.css') }}">
+        <link rel="stylesheet"
+            href="{{ asset('TAssets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+    </x-slot>
+
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <span id="success" {{ session('success') ? 'data-success = true' : false }}
+            data-success-message='{{ json_encode(session('success')) }}'></span>
+        <span id="error" {{ session('error') ? 'data-error = true' : false }}
+            data-error-message='{{ json_encode(session('error')) }}'></span>
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Attendance</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url()->previous() }}">Back</a></li>
+                            <li class="breadcrumb-item active">Attendance</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <!-- Default box -->
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Attendance</h3>
+                            </div>
+                            <form method="POST"
+                                action="{{ route('attendance.store', ['student' => $student, 'termId' => $term->id]) }}">
+                                @csrf
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="">Number of Times <span
+                                                class="text-lg">{{ $student->first_name }}
+                                                {{ $student->last_name }}</span> was present in <span
+                                                class="text-lg">{{ $term->name }}
+                                                {{ $academicSession->name }}</span></label>
+                                        <input type="number" name="value"
+                                            class="form-control @error('value') is-invalid @enderror"
+                                            value="{{ old('value') }}" required>
+                                        @error('value')
+                                            <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+
+                                </div>
+                                <!-- /.card-body -->
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+        </section>
+        <!-- /.content -->
+    </div>
+    <x-slot name="scripts">
+        <!-- Toastr -->
+        <script src="{{ asset('TAssets/plugins/toastr/toastr.min.js') }}"></script>
+        <!-- Select2 -->
+        <script src="{{ asset('TAssets/plugins/select2/js/select2.full.min.js') }}"></script>
+        <!-- AdminLTE App -->
+        <script>
+            $(function() {
+                let Success = document.getElementById('success')
+                let Error = document.getElementById('error')
+
+                // if data-success = 'true' display alert
+                if (Success.dataset.success == 'true')
+                    $(document).Toasts('create', {
+                        class: 'bg-success',
+                        title: 'Success',
+                        subtitle: 'Close',
+                        body: JSON.parse(Success.dataset.successMessage)
+                    })
+
+                if (Error.dataset.error == 'true')
+                    $(document).Toasts('create', {
+                        class: 'bg-danger',
+                        title: 'Error',
+                        subtitle: 'Close',
+                        body: JSON.parse(Error.dataset.errorMessage)
+                    })
+
+
+
+            });
+            $(function() {
+
+                //Initialize Select2 Elements
+                $('.select2').select2()
+            })
+
+        </script>
+    </x-slot>
+</x-app-layout>
