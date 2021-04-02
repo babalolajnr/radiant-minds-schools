b2fb5784ef282e47d15ff82488a00f6f10ec6bce
babalolajnr <babalolajnr@gmail.com>
2021-04-02 19:17:42 +0100

    added test_user_password_update_method

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -14,13 +14,27 @@ class UserTest extends TestCase
 
     public function test_user_update_method()
     {
-        // $this->withoutExceptionHandling();
         $user = User::factory()->create();
         $response = $this->actingAs($user)->patch(route('user.update', ['user' => $user]), [
             'first_name' => $this->faker->firstName,
             'last_name' => $user->last_name,
             'email' => $user->email
         ]);
+        $response->assertStatus(302)->assertSessionHas('success');
+    }
+
+    public function test_user_password_update_method()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->patch(route('user.update.password', ['user' => $user]), [
+            'current_password' => '11111111', //default user factory password(8 ones)
+            'new_password' => 'ANewPassword',
+            'new_password_confirmation' => 'ANewPassword'
+        ]);
+
         $response->assertStatus(302)->assertSessionHas('success');
     }
 }
