f7fff4b2430199c3bf44d33c913083181ed5e5c3
babalolajnr <babalolajnr@gmail.com>
2021-04-04 15:54:14 +0100

    added chart

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -145,6 +145,65 @@
 
                     <!-- ./col -->
                 </div>
+                <div class="row">
+                    <section class="col-lg-7 connectedSortable ui-sortable">
+                        <!-- Custom tabs (Charts with tabs)-->
+                        <div class="card">
+                            <div class="card-header ui-sortable-handle" style="cursor: move;">
+                                <h3 class="card-title">
+                                    <i class="fas fa-chart-pie mr-1"></i>
+                                    Sales
+                                </h3>
+                                <div class="card-tools">
+                                    <ul class="nav nav-pills ml-auto">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#revenue-chart" data-toggle="tab">Area</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link active" href="#sales-chart" data-toggle="tab">Donut</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div><!-- /.card-header -->
+                            <div class="card-body">
+                                <div class="tab-content p-0">
+                                    <!-- Morris chart - Sales -->
+                                    <div class="chart tab-pane" id="revenue-chart"
+                                        style="position: relative; height: 300px;">
+                                        <div class="chartjs-size-monitor">
+                                            <div class="chartjs-size-monitor-expand">
+                                                <div class=""></div>
+                                            </div>
+                                            <div class="chartjs-size-monitor-shrink">
+                                                <div class=""></div>
+                                            </div>
+                                        </div>
+                                        <canvas id="revenue-chart-canvas" height="300"
+                                            style="height: 300px; display: block; width: 578px;" width="578"
+                                            class="chartjs-render-monitor"></canvas>
+                                    </div>
+                                    <div class="chart tab-pane active" id="sales-chart"
+                                        style="position: relative; height: 300px;">
+                                        <div class="chartjs-size-monitor">
+                                            <div class="chartjs-size-monitor-expand">
+                                                <div class=""></div>
+                                            </div>
+                                            <div class="chartjs-size-monitor-shrink">
+                                                <div class=""></div>
+                                            </div>
+                                        </div>
+                                        <canvas id="myChart" height="300"
+                                            style="height: 300px; display: block; width: 578px;" width="578"
+                                            class="chartjs-render-monitor"></canvas>
+
+                                    </div>
+                                </div>
+                            </div><!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                        <!-- /.card -->
+                    </section>
+                </div>
                 <!-- /.row -->
                 <!-- Main row -->
 
@@ -154,6 +213,34 @@
         <!-- /.content -->
     </div>
     <x-slot name="scripts">
+        <!-- ChartJS -->
+        {{-- <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script> --}}
+        <script src="{{ asset('TAssets/plugins/chart.js/Chart.bundle.min.js') }}"></script>
+        <script>
+            const ctx = document.getElementById('myChart').getContext('2d');
+            const data = {
+                labels: [
+                    'Red',
+                    'Blue',
+                    'Yellow'
+                ],
+                datasets: [{
+                    label: 'Classroom Population',
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(54, 162, 235)',
+                        'rgb(255, 205, 86)'
+                    ],
+                    hoverOffset: 4
+                }]
+            };
+            var myChart = new Chart(ctx, {
+                type: 'pie',
+                data: data,
+                
+            });
 
+        </script>
     </x-slot>
 </x-app-layout>
