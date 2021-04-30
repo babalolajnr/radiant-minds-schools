65e0704c259ec866b0200a254b6a08153bebb20d
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-04-30 10:30:07 +0100

    created fee

---
+++ database/factories/FeeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Fee;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class FeeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Fee::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_04_30_091936_create_fees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fees', function (Blueprint $table) {
+            $table->id();
+            $table->double('fee');
+            $table->foreignId('academic_session_term_id')->constrained('academic_session_term')->onUpdate('cascade')->onDelete('restrict');
+            $table->foreignId('classroom_id')->constrained('classrooms')->onUpdate('cascade')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fees');
+    }
+}
