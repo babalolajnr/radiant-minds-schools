16384492ceab04eeb313d0065664ee0d0fa39de8
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-04-30 11:03:01 +0100

    added relationships and guarded property

--- app/Models/Fee.php
+++ app/Models/Fee.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Fee extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function academicSessionTerm()
+    {
+        return $this->hasOne(AcademicSessionTerm::class);
+    }
+
+    public function classroom()
+    {
+        return $this->hasOne(Classroom::class);
+    }
+
 }
