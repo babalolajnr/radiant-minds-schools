9774a6a6896ce307700fd0060684567c3ab53571
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-01 15:34:31 +0100

    added relationships

--- app/Models/AcademicSessionTerm.php
+++ app/Models/AcademicSessionTerm.php
@@ -5,6 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * AcademicSessionTerm is also referred to as a 'Period'
+ * so wherever period is seen it also means AcademicSessionTerm
+ * 
+ * This model also serves as the pivot table of 'academic_sessions' and
+ * 'terms' table
+ */
 class AcademicSessionTerm extends Model
 {
     use HasFactory;
@@ -23,5 +30,39 @@ class AcademicSessionTerm extends Model
     public function fee()
     {
         return $this->belongsTo(Fee::class);
+    }
+
+    public function attendance()
+    {
+        return $this->belongsTo(Attendance::class);
+    }
+
+    public function term()
+    {
+        return $this->belongsTo(Term::class);
+    }
+
+    public function academicSession()
+    {
+        return $this->belongsTo(AcademicSession::class);
+    }
+
+    /**
+     * checks if period is active
+     * @return bool
+     */
+    public function isActive()
+    {
+        return $this->active == true;
+    }
+
+    /**
+     * gets active period
+     * @return AcademicSessionTerm
+     */
+    public static function activePeriod()
+    {
+        $activePeriod = AcademicSessionTerm::where('active', true)->first();
+        return $activePeriod;
     }
 }
