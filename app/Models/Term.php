b4361d193904809e474fb66e210a731a7742ed78
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-02 09:55:07 +0100

    removed results relationship

--- app/Models/Term.php
+++ app/Models/Term.php
@@ -10,20 +10,10 @@ class Term extends Model
     use HasFactory;
     protected $fillable = ['name', 'slug'];
 
-    public function Results()
-    {
-        return $this->hasMany(Result::class);
-    }
-
     public function pds()
     {
         return $this->hasMany(PD::class);
     }
-
-    // public function academic_sessions()
-    // {
-    //     return $this->belongsToMany(AcademicSession::class)->using(Period::class)->withPivot('start_date', 'end_date')->withTimestamps();
-    // }
 
     public function periods()
     {
