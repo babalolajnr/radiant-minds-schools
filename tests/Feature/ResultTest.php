432c41009817913b1568fb4b1ab125e3d9ef2e5a
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-02 09:57:51 +0100

    refactored test_user_can_store_results to work with period relationship

--- tests/Feature/ResultTest.php
+++ tests/Feature/ResultTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\AcademicSession;
+use App\Models\Period;
 use App\Models\Result;
 use App\Models\Student;
 use App\Models\Subject;
@@ -17,18 +18,17 @@ class ResultTest extends TestCase
 
     public function test_user_can_store_results()
     {
+        $this->withoutExceptionHandling();
         //create an academic session for the test
-        AcademicSession::factory()->create(['current_session' => 1]);
+        Period::factory()->create(['active' => true]);
 
         $user = User::factory()->create();
         $student = Student::factory()->create();
         $subject = Subject::factory()->create();
-        $term = Term::factory()->create();
 
         $response = $this->actingAs($user)->post(route('result.store', ['student' => $student]), [
             'ca' => mt_rand(0, 40),
             'exam' => mt_rand(0, 60),
-            'term' => $term->name,
             'subject' => $subject->name
         ]);
 
