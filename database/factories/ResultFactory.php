6ffde70401cddc5407e4d7c2144b64cacc41d710
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-02 10:00:03 +0100

    removed term and academic_session_id and added period_id

--- database/factories/ResultFactory.php
+++ database/factories/ResultFactory.php
@@ -2,13 +2,11 @@
 
 namespace Database\Factories;
 
-use App\Models\AcademicSession;
+use App\Models\Period;
 use App\Models\Result;
 use App\Models\Student;
 use App\Models\Subject;
-use App\Models\Term;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Artisan;
 
 class ResultFactory extends Factory
 {
@@ -30,13 +28,12 @@ class ResultFactory extends Factory
         $ca = mt_rand(0, 40);
         $exam = mt_rand(0, 60);
         return [
-            'term_id' => Term::factory()->create()->id,
-            'academic_session_id' => AcademicSession::factory()->create()->id,
             'subject_id' => Subject::factory()->create()->id,
             'student_id' => Student::factory()->create()->id,
             'ca' => $ca,
             'exam' => $exam,
-            'total' => $exam + $ca
+            'total' => $exam + $ca,
+            'period_id' => Period::factory()->create()->id,
         ];
     }
 
