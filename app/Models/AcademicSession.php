0b233eb5a4fe6e9791e0dc6e01ca6d5bf3eb751c
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-02 09:37:35 +0100

    removed results relationship

--- app/Models/AcademicSession.php
+++ app/Models/AcademicSession.php
@@ -11,16 +11,10 @@ class AcademicSession extends Model
 
     protected $fillable = ['name', 'slug', 'start_date', 'end_date'];
 
-    public function results()
-    {
-        return $this->hasMany(Result::class);
-    }
-
     public function pds()
     {
         return $this->hasMany(PD::class);
     }
-
 
     public function periods()
     {
