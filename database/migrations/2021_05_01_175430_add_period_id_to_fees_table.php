fedd5eba29f261cef143e3d7637ba382b4445a4f
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-02 08:27:32 +0100

    added period_id to fees table

---
+++ database/migrations/2021_05_01_175430_add_period_id_to_fees_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPeriodIdToFeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fees', function (Blueprint $table) {
+            $table->foreignId('period_id')->constrained('periods')->onUpdate('cascade')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('fees', function (Blueprint $table) {
+            //
+        });
+    }
+}
