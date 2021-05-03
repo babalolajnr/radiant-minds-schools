7baf6569d3412f648c7e059fa88cf1789eb4cba6
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-03 10:12:07 +0100

    added test_period_can_be_stored_when_there_are_other_period_records

--- tests/Feature/PeriodTest.php
+++ tests/Feature/PeriodTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\AcademicSession;
+use App\Models\Period;
 use App\Models\Term;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -18,6 +19,26 @@ class PeriodTest extends TestCase
     {
         $this->withoutExceptionHandling();
         $user = User::factory()->create(['user_type' => 'master']);
+
+        $response = $this->actingAs($user)->post(
+            route('period.store'),
+            [
+                'academic_session' => AcademicSession::factory()->create()->name,
+                'term' => Term::factory()->create()->name,
+                'start_date' => now(),
+                'end_date' => now()->addMonths(4)
+            ]
+        );
+
+        $response->assertStatus(302)->assertSessionHas('success');
+    }
+
+    public function test_period_can_be_stored_when_there_are_other_period_records()
+    {
+        $this->withoutExceptionHandling();
+        $user = User::factory()->create(['user_type' => 'master']);
+
+        Period::factory()->create();
 
         $response = $this->actingAs($user)->post(
             route('period.store'),
