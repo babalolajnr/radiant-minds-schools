53567e8708a183813acb4f99cd631da306867a7f
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-03 07:18:43 +0100

    added comments

--- app/Models/ADType.php
+++ app/Models/ADType.php
@@ -8,13 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class ADType extends Model
 {
     use HasFactory;
-
+    
+    /**
+     * fillable
+     *
+     * @var array
+     */
     protected $fillable = ['name', 'slug'];
-
+    
+    /**
+     * casts
+     *
+     * @var array
+     */
     protected $casts = [
         'slug' => 'string'
     ];
-
+    
+    /**
+     * ads relationship
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function ads()
     {
         return $this->hasMany(AD::class);
