81ef5f0da45c22fbb6eed994dd3154676e19ad0e
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-03 05:25:02 +0100

    added test_ad_can_be_stored

--- tests/Feature/ADTest.php
+++ tests/Feature/ADTest.php
@@ -2,11 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\ADType;
 use App\Models\Period;
 use App\Models\Student;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ADTest extends TestCase
@@ -37,5 +37,28 @@ class ADTest extends TestCase
         $response = $this->actingAs($user)->get(route('ad.create', ['student' => $student]));
 
         $response->assertStatus(200);
+    }
+
+    public function test_ad_can_be_stored()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = User::factory()->create();
+        $student = Student::factory()->create();
+        $period = Period::factory()->create();
+
+        $adTypes = ADType::factory()->times(5)->create();
+        $adTypes = $adTypes->pluck('slug')->all();
+        $data = [];
+        
+        foreach ($adTypes as $adType) {
+            $data += [$adType => mt_rand(1, 5)];
+        }
+
+        $response = $this->actingAs($user)->post(route('ad.storeOrUpdate', ['student' => $student, 'periodSlug' => $period->slug]), [
+            'adTypes' => $data
+        ]);
+
+        $response->assertStatus(302)->assertSessionHas('success');
     }
 }
