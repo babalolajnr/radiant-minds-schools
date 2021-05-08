7bfe0a0e0315865f61b01efd69f0dbf82cce2fb1
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-08 14:22:00 +0100

    added store method

--- app/Http/Controllers/FeeController.php
+++ app/Http/Controllers/FeeController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Classroom;
+use App\Models\Fee;
+use App\Models\Period;
 use Illuminate\Http\Request;
 
 class FeeController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'classroom' => ['required', 'string', 'exists:classrooms,name'],
+            'fee' => ['required', 'string', 'numeric'],
+            'period' => ['required', 'string', 'exists:periods,slug']
+        ]);
+
+        $classroom = Classroom::where('name', $data['classroom'])->first();
+        $period = Period::where('slug', $data['period'])->first();
+
+        //check if record exists
+        $row = Fee::where('classroom_id', $classroom->id)->where('period_id', $period->id);
+
+        if ($row->exists()){
+            return back()->with('error', 'Record Exists');
+        }
+
+        Fee::create([
+            'classroom_id' => $classroom->id,
+            'fee' => $data['fee'],
+            'period_id' => $period->id,
+        ]);
+
+        return back()->with('success', 'Record created');
+    }
 }
