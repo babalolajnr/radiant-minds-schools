9efcf25c9ca7b69210344403f672b015fbb3a360
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-09 17:28:38 +0100

    changed header to periods

--- resources/views/periods.blade.php
+++ resources/views/periods.blade.php
@@ -24,7 +24,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Academic Sessions</h1>
+                        <h1>Periods</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
