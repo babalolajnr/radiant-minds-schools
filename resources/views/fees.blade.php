0cdc21e519023f4f0c2a8933cb83eac004d20e52
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-10 12:06:14 +0100

    created fees view

---
+++ resources/views/fees.blade.php
@@ -0,0 +1,248 @@
+<x-app-layout>
+    <x-slot name="styles">
+        <!-- Toastr -->
+        <link rel="stylesheet" href="{{ asset('TAssets/plugins/toastr/toastr.min.css') }}">
+        <!-- DataTables -->
+        <link rel="stylesheet" href="{{ asset('TAssets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+        <link rel="stylesheet"
+            href="{{ asset('TAssets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+        <link rel="stylesheet"
+            href="{{ asset('TAssets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+
+    </x-slot>
+
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <span id="success" {{ session('success') ? 'data-success = true' : false }}
+            data-success-message='{{ json_encode(session('success')) }}'></span>
+        <span id="error" {{ session('error') ? 'data-error = true' : false }}
+            data-error-message='{{ json_encode(session('error')) }}'></span>
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Fees</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url()->previous() }}">Back</a></li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+
+                        <!-- Default box -->
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">New Fee</h3>
+                            </div>
+                            <form id="" method="POST" action="{{ route('fee.store') }}">
+                                @csrf
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="amount">Amount</label>
+                                        <input type="number" name="amount" class="form-control"
+                                            value={{ old('amount') }}>
+                                        @error('amount')
+                                            <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="Classroom">Classroom</label>
+                                        <select class="form-control select2" name="classroom" style="width: 100%;">
+                                            @foreach ($classrooms as $classroom)
+                                                <option @if (old('classroom') == $classroom) SELECTED @endif>{{ $classroom->name }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                        @error('classroom')
+                                            <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="Period">Period</label>
+                                        <select class="form-control select2" name="period" style="width: 100%;">
+                                            @foreach ($periods as $period)
+                                                <option @if (old('period') == $period) SELECTED @endif
+                                                    value="{{ $period->slug }}">{{ $period->term->name }}
+                                                    {{ $period->academicSession->name }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                        @error('period')
+                                            <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+
+                                </div>
+                                <!-- /.card-body -->
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Fee record</h3>
+                            </div>
+                            <div class="card-body">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Period</th>
+                                            <th>Classroom</th>
+                                            <th>Amount</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($fees as $fee)
+                                            <tr>
+                                                <td>
+                                                    {{ $fee->period->term->name }} {{ $fee->period->academicSession->name }}
+                                                </td>
+                                                <td>
+                                                    {{ $fee->classroom->name }}
+                                                </td>
+                                                <td>
+                                                    {{ $fee->amount }}
+                                                </td>
+                                                <td>
+                                                    <div class="btn-group">
+                                                        <a href="{{ route('fee.edit', ['fee' => $fee]) }}">
+                                                            <button type="button" class="btn btn-default btn-flat"
+                                                                title="Edit">
+                                                                <i class="fa fa-edit"></i>
+                                                            </button>
+                                                        </a>
+                                                        <button type="button" class="btn btn-danger btn-flat"
+                                                            title="Delete"
+                                                            onclick="deleteConfirmationModal('{{ route('fee.destroy', ['fee' => $fee]) }}', '{{ $fee->name }}')">
+                                                            <i class="fas fa-trash"></i>
+                                                        </button>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Period</th>
+                                            <th>Classroom</th>
+                                            <th>Amount</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </section>
+        <!-- /.content -->
+    </div>
+    <div class="modal fade" id="deleteConfirmationModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Confirmation</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to delete <span id="deleteItemName" class="font-bold"></span>?
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <form action="" method="POST" id="yesDeleteConfirmation">
+                        @method('DELETE')
+                        @csrf
+                        <button type="submit" class="btn btn-danger">Yes</button>
+                    </form>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+
+    <x-slot name="scripts">
+        <!-- Toastr -->
+        <script src="{{ asset('TAssets/plugins/toastr/toastr.min.js') }}"></script>
+        <!-- DataTables  & Plugins -->
+        <script src="{{ asset('TAssets/plugins/datatables/jquery.dataTables.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/jszip/jszip.min.js') }}"></script>
+        <script src="{{ asset('TAssets/plugins/pdfmake/pdfmake.min.js') }}"></script>
+        <script src="{{ asset('TAssets/plugins/pdfmake/vfs_fonts.js') }}"></script>
+        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/buttons.html5.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/buttons.print.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/datatables-buttons/js/buttons.colVis.min.js') }}">
+        </script>
+        <script src="{{ asset('TAssets/plugins/moment/moment.min.js') }}"></script>
+
+        <!-- AdminLTE App -->
+        <script>
+            function deleteConfirmationModal(url, name) {
+                $('#yesDeleteConfirmation').attr("action", url)
+                $('#deleteItemName').html(name)
+                $('#deleteConfirmationModal').modal('show')
+            }
+
+            $(function() {
+                let Success = document.getElementById('success')
+                let Error = document.getElementById('error')
+
+                // if data-success = 'true' display alert
+                if (Success.dataset.success == 'true')
+                    $(document).Toasts('create', {
+                        class: 'bg-success',
+                        title: 'Success',
+                        subtitle: 'Close',
+                        body: JSON.parse(Success.dataset.successMessage)
+                    })
+                if (Error.dataset.error == 'true')
+                    $(document).Toasts('create', {
+                        class: 'bg-danger',
+                        title: 'Error',
+                        subtitle: 'Close',
+                        body: JSON.parse(Error.dataset.errorMessage)
+                    })
+            });
+
+            $(function() {
+                $("#example1").DataTable({
+                    "responsive": true,
+                    "lengthChange": false,
+                    "autoWidth": false,
+                    "buttons": ["copy", "csv", "excel", "pdf", "print"]
+                }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+
+            });
+
+        </script>
+    </x-slot>
+</x-app-layout>
