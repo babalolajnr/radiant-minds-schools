e4b3142376557dbc1e1c06508a467b5aa19e9e4c
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-11 13:03:16 +0100

    optimized academicSesionSeeder

--- database/seeders/AcademicSessionSeeder.php
+++ database/seeders/AcademicSessionSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\AcademicSession;
+use Carbon\Carbon;
 use Database\Factories\AcademicSessionFactory;
 use Illuminate\Database\Seeder;
 
@@ -16,12 +17,40 @@ class AcademicSessionSeeder extends Seeder
     public function run()
     {
 
-        $academicSessions = AcademicSession::all();
-        if (count($academicSessions) < 1) {
-            
-            $academicSessions = AcademicSessionFactory::$academicSessions;
+        $this->command->getOutput()->progressStart(100);
 
-            AcademicSession::factory()->times(count($academicSessions))->create();
+        $academicSessions = AcademicSessionFactory::$academicSessions;
+
+        foreach ($academicSessions as $academicSession) {
+            $record = AcademicSession::where('name', $academicSession);
+
+            if ($record->exists()) {
+                continue;
+            }
+
+            /**
+             * break the string and extract the first part before the '-'
+             * then generate a random day and month
+             */
+            $eachYear = explode("-", $academicSession);
+            $startYear = $eachYear[0];
+
+            $startDate = Carbon::now();
+            $startDate->year($startYear);
+            $startDate->month(mt_rand(1, 12));
+            $startDate->day(mt_rand(1, 30));
+
+            $endDate = Carbon::createFromFormat('Y-m-d', $startDate->toDateString())->addYear();
+
+            AcademicSession::create([
+                'name' => $academicSession,
+                'start_date' => $startDate,
+                'end_date' => $endDate,
+            ]);
+            
+            $this->command->getOutput()->progressAdvance();
         }
+
+        $this->command->getOutput()->progressFinish();
     }
 }
