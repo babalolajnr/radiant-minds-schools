524778a394a6ea0d8e55662d6a7337ccfa111aa5
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-13 21:29:33 +0100

    added teacher authentication tests

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Teacher;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -45,4 +46,31 @@ class AuthenticationTest extends TestCase
 
         $this->assertGuest();
     }
+
+    public function test_teachers_can_authenticate_using_the_login_screen()
+    {
+        $this->withoutExceptionHandling();
+        $teacher = Teacher::factory()->create();
+
+        $response = $this->post('/login', [
+            'email' => $teacher->email,
+            'password' => '11111111',
+        ]);
+
+        $this->assertAuthenticated('webteachers');
+        $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function test_teacher_can_not_authenticate_with_invalid_password()
+    {
+        $teacher = Teacher::factory()->create();
+
+        $this->post('/login', [
+            'email' => $teacher->email,
+            'password' => 'wrong-password',
+        ]);
+
+        $this->assertGuest();
+    }
+
 }
