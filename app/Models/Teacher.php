cd9d900f82cd54fbccd5b42939827c0c5cc9a691
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-13 21:27:41 +0100

    modified teacher to an authenticatable model

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -3,21 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Teacher extends Model
+class Teacher extends Authenticatable
 {
-    use HasFactory;
-    use SoftDeletes;
-    
+    use HasFactory, SoftDeletes,  Notifiable;
+
     /**
      * The attributes that are not mass assignable.
      *
      * @var array
      */
-    protected $guarded = ['id', 'created_at', 'updated_at'];
-    
+    protected $guarded = ['id', 'created_at', 'updated_at', 'email_verified_at', 'remember_token'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
     /**
      * Classroom relationship
      *
@@ -27,7 +46,7 @@ class Teacher extends Model
     {
         return $this->hasOne(Classroom::class);
     }
-    
+
     /**
      * Check if teacher is active
      *
