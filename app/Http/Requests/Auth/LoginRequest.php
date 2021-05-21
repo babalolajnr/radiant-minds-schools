abf588f9e0e8745a1ed4d05c52f85de8dad4f5ec
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-21 14:55:07 +0100

    removed teacher login attempt

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -46,14 +46,12 @@ class LoginRequest extends FormRequest
         $this->ensureIsNotRateLimited();
 
         if (!Auth::attempt($this->only('email', 'password'), $this->filled('remember'))) {
-            if (!Auth::guard('teacher')->attempt($this->only('email', 'password', $this->filled('remember')))) {
 
-                RateLimiter::hit($this->throttleKey());
+            RateLimiter::hit($this->throttleKey());
 
-                throw ValidationException::withMessages([
-                    'email' => __('auth.failed'),
-                ]);
-            }
+            throw ValidationException::withMessages([
+                'email' => __('auth.failed'),
+            ]);
         }
 
         RateLimiter::clear($this->throttleKey());
