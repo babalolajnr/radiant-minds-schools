7b405636520107b4899394442a8ab2ecf34dcc59
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-22 08:03:38 +0100

    fixed user logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -43,7 +43,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request)
     {
-        $request->logout();
+        Auth::logout();
 
         $request->session()->invalidate();
 
