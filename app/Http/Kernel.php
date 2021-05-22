1a76d8342da53a06c73dfdc321f7eac0129bc81d
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-22 08:03:05 +0100

    added isAdminOrMaster route middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'isActiveAndVerified' => \App\Http\Middleware\CheckUserIsActiveAndVerified::class,
+        'isAdminOrMaster' => \App\Http\Middleware\CheckUserIsAdminOrMaster::class
     ];
 }
