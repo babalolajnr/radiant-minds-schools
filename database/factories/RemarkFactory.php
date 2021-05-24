b140d84677bec5696e58fe80b8c53d69ee0a1caa
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-24 11:50:22 +0100

    created Remark...

---
+++ database/factories/RemarkFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Remark;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RemarkFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Remark::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
