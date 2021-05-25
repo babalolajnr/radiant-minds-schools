2d128c0584f16b687cf1debeee57605f73c8d065
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-25 12:42:37 +0100

    added fields

--- database/factories/RemarkFactory.php
+++ database/factories/RemarkFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Period;
 use App\Models\Remark;
+use App\Models\Student;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class RemarkFactory extends Factory
@@ -22,7 +24,10 @@ class RemarkFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'class_teacher_remark' => $this->faker->realText(),
+            'hos_remark' => $this->faker->realText(),
+            'student_id' => Student::factory()->create()->id,
+            'period_id' => Period::factory()->create()->id
         ];
     }
 }
