6e853e603c9926186b95db9c69de3fb7e52cd508
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-25 12:40:53 +0100

    added guarded property and relationships

--- app/Models/Remark.php
+++ app/Models/Remark.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Remark extends Model
 {
     use HasFactory;
+    
+    /**
+     * Columns that are not mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+    
+    /**
+     * student relationship
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+    
+    /**
+     * period relationship
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function period()
+    {
+        return $this->belongsTo(Period::class);
+    }
+
+    
 }
