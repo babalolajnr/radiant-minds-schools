7592f4f5e3847196b845ab2925143f2fe8ec1077
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-27 15:34:22 +0100

    added seeder logic

--- database/seeders/HosRemarkSeeder.php
+++ database/seeders/HosRemarkSeeder.php
@@ -2,7 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\HosRemark;
+use App\Models\Period;
+use App\Models\Student;
+use App\Models\User;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class HosRemarkSeeder extends Seeder
 {
@@ -13,6 +19,58 @@ class HosRemarkSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $this->command->getOutput()->progressStart(100);
+        
+        $data = $this->allRecords();
+        $faker = Factory::create();
+
+        $user = User::factory()->create(['user_type' => 'master']);
+
+        foreach ($data['students'] as $student) {
+
+            foreach ($data['periods'] as $period) {
+
+                $record = HosRemark::where('student_id', $student->id)
+                    ->where('period_id', $period->id);
+
+                if ($record->exists()) {
+                    continue;
+                }
+
+                HosRemark::create([
+                    'student_id' => $student->id,
+                    'period_id' => $period->id,
+                    'user_id' => $user->id,
+                    'remark' => $faker->realText()
+                ]);
+            }
+
+            $this->command->getOutput()->progressAdvance();
+        }
+
+        $this->command->getOutput()->progressFinish();
+    }
+
+    private function allRecords()
+    {
+        $period = Period::first();
+        $student = Student::first();
+
+        //if any of the required values are empty seed their tables
+        if (is_null($period)) {
+            Artisan::call('db:seed', ['--class' => 'PeriodSeeder']);
+        }
+
+        if (is_null($student)) {
+            Artisan::call('db:seed', ['--class' => 'StudentSeeder']);
+        }
+
+        $periods = Period::all();
+        $students = Student::all();
+
+        return [
+            'periods' => $periods,
+            'students' => $students,
+        ];
     }
 }
