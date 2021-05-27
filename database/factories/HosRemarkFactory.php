aafa5c9aaa19fea4f64013013c75e41bbd81e503
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-27 15:34:01 +0100

    added fields

--- database/factories/HosRemarkFactory.php
+++ database/factories/HosRemarkFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use App\Models\HosRemark;
+use App\Models\Period;
+use App\Models\Student;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class HosRemarkFactory extends Factory
@@ -22,7 +25,10 @@ class HosRemarkFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'remark' => $this->faker->realText(),
+            'student_id' => Student::factory()->create()->id,
+            'period_id' => Period::factory()->create(['active' => true])->id,
+            'user_id' => User::factory()->create()->id,
         ];
     }
 }
