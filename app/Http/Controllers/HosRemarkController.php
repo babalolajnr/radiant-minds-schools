943a9af6c259d2f117833f19ffe541fc74f0448d
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-27 14:52:39 +0100

    created hosremark

---
+++ app/Http/Controllers/HosRemarkController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HosRemark;
+use Illuminate\Http\Request;
+
+class HosRemarkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\HosRemark  $hosRemark
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HosRemark $hosRemark)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\HosRemark  $hosRemark
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HosRemark $hosRemark)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\HosRemark  $hosRemark
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, HosRemark $hosRemark)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\HosRemark  $hosRemark
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HosRemark $hosRemark)
+    {
+        //
+    }
+}
---
+++ database/factories/HosRemarkFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\HosRemark;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class HosRemarkFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = HosRemark::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
