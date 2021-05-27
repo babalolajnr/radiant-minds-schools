be4ed95e09ed272d53019af2c5ff62fb3af2caa2
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-27 15:34:37 +0100

    creatde hos remark view

---
+++ resources/views/createHosRemark.blade.php
@@ -0,0 +1,115 @@
+<x-app-layout>
+    <x-slot name="styles">
+        <!-- Toastr -->
+        <link rel="stylesheet" href="{{ asset('TAssets/plugins/toastr/toastr.min.css') }}">
+        <!-- Select2 -->
+        <link rel="stylesheet" href="{{ asset('TAssets/plugins/select2/css/select2.min.css') }}">
+        <link rel="stylesheet"
+            href="{{ asset('TAssets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+    </x-slot>
+
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <span id="success" {{ session('success') ? 'data-success = true' : false }}
+            data-success-message='{{ json_encode(session('success')) }}'></span>
+        <span id="error" {{ session('error') ? 'data-error = true' : false }}
+            data-error-message='{{ json_encode(session('error')) }}'></span>
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>HOS Remark</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        {{-- <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url()->previous() }}">Back</a></li>
+                            <li class="breadcrumb-item active">HOS Remark</li>
+                        </ol> --}}
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <!-- Default box -->
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Create/Update HOS Remark</span></h3>
+                            </div>
+                            <form method="POST"
+                                action="{{ route('remark.teacher.storeOrUpdate', ['student' => $student]) }}">
+                                @csrf
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="HOS remark">HOS Remark</label>
+                                        <textarea class='form-control @error('remark') is-invalid
+                                            @enderror' name='remark' rows="4">
+                                                @if (!is_null($remark))
+                                                    {{ old('remark', $remark->remark) }}
+                                                @else 
+                                                    {{ old('remark') }}
+                                                @endif
+                                            </textarea>
+
+                                        @error('remark')
+                                            <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+        </section>
+        <!-- /.content -->
+    </div>
+    <x-slot name="scripts">
+        <!-- Toastr -->
+        <script src="{{ asset('TAssets/plugins/toastr/toastr.min.js') }}"></script>
+        <!-- Select2 -->
+        <script src="{{ asset('TAssets/plugins/select2/js/select2.full.min.js') }}"></script>
+        <!-- AdminLTE App -->
+        <script>
+            $(function() {
+                let Success = document.getElementById('success')
+                let Error = document.getElementById('error')
+
+                // if data-success = 'true' display alert
+                if (Success.dataset.success == 'true')
+                    $(document).Toasts('create', {
+                        class: 'bg-success',
+                        title: 'Success',
+                        subtitle: 'Close',
+                        body: JSON.parse(Success.dataset.successMessage)
+                    })
+
+                if (Error.dataset.error == 'true')
+                    $(document).Toasts('create', {
+                        class: 'bg-danger',
+                        title: 'Error',
+                        subtitle: 'Close',
+                        body: JSON.parse(Error.dataset.errorMessage)
+                    })
+
+
+
+            });
+            $(function() {
+
+                //Initialize Select2 Elements
+                $('.select2').select2()
+            })
+
+        </script>
+    </x-slot>
+</x-app-layout>
