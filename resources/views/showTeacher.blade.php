0cef552edd74753bd49c89e2fcdb9530c7a2e3af
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-27 09:56:39 +0100

    refactored show teacher view for conditional rendering depending on the type of user

--- resources/views/showTeacher.blade.php
+++ resources/views/showTeacher.blade.php
@@ -54,12 +54,22 @@
 
                                     <p class="text-muted text-center" id="admissionNo">{{ $teacher->admission_no }}
                                     </p>
-                                    <div class="d-flex justify-content-center">
-                                        <a href="{{ route('teacher.edit', ['teacher' => $teacher]) }}">
-                                            <button class="btn btn-primary" data-toggle="modal"
-                                                data-target="#editModal">Edit</button>
-                                        </a>
-                                    </div>
+                                    @auth('teacher')
+                                        <div class="d-flex justify-content-center">
+                                            <a href="{{ route('teacher.edit', ['teacher' => $teacher]) }}">
+                                                <button class="btn btn-primary" data-toggle="modal"
+                                                    data-target="#editModal">Edit</button>
+                                            </a>
+                                        </div>
+                                    @endauth
+
+                                    @if ($teacher->signature != null)
+                                        <div class="d-flex justify-content-center pt-4">
+                                            <img src="{{ asset($teacher->signature) }}" class="img-responsive"
+                                                height="30" width="50" alt="teacher' signature">
+                                        </div>
+                                    @endif
+
                                 </div>
                                 <!-- /.card-body -->
                             </div>
@@ -73,16 +83,49 @@
                                     <ul class="nav nav-pills">
                                         <li class="nav-item"><a class="nav-link active" href="#about"
                                                 data-toggle="tab">About</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="#status"
-                                                data-toggle="tab">Status</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="#imageUpload"
-                                                data-toggle="tab">Image Upload</a></li>
+                                        @auth('web')
+                                            <li class="nav-item"><a class="nav-link" href="#status"
+                                                    data-toggle="tab">Status</a></li>
+                                        @endauth
+                                        @auth('teacher')
+                                            <li class="nav-item"><a class="nav-link" href="#signature"
+                                                    data-toggle="tab">Signature Upload</a></li>
+                                        @endauth
                                     </ul>
 
                                 </div><!-- /.card-header -->
                                 <div class="card-body">
                                     <div class="tab-content">
                                         <div class="active tab-pane" id="about">
+                                            <strong></i>First name</strong>
+
+                                            <p class="text-muted" id="first_name">{{ $teacher->first_name }}</p>
+
+                                            <hr>
+
+                                            <strong></i>Last name</strong>
+
+                                            <p class="text-muted" id="last_name">{{ $teacher->last_name }}</p>
+
+                                            <hr>
+
+                                            <strong></i>Sex</strong>
+
+                                            <p class="text-muted" id="sex">{{ $teacher->sex }}</p>
+
+                                            <hr>
+
+                                            <strong></i>Phone number</strong>
+
+                                            <p class="text-muted" id="phone_number">{{ $teacher->phone }}</p>
+
+                                            <hr>
+
+                                            <strong></i>Email</strong>
+
+                                            <p class="text-muted" id="email">{{ $teacher->email }}</p>
+
+                                            <hr>
                                             <strong></i>Class</strong>
 
                                             @if ($teacher->classroom)
@@ -92,6 +135,8 @@
                                                         {{ $teacher->classroom->name }}
                                                     </p>
                                                 </a>
+                                            @else
+                                                <p class="text-muted">-</p>
                                             @endif
 
                                             <hr>
@@ -104,69 +149,76 @@
 
                                         <!-- /.tab-pane -->
 
-                                        <div class="tab-pane" id="status">
-                                            <div class="form-group row">
-                                                {{-- Status --}}
-                                                <label for="status" class="col-sm-2 col-form-label">Status</label>
-                                                <div class="col-sm-10">
-                                                    <div class="btn-group">
-                                                        <form
-                                                            action="{{ route('teacher.activate', ['teacher' => $teacher]) }}"
-                                                            method="post">
-                                                            @csrf
-                                                            @method('PATCH')
-                                                            <button type="submit" class="btn @if ($teacher->isActive()) btn-primary
-                                                            disabled @else btn-default @endif
-                                                                btn-flat"
-                                                                @if ($teacher->isActive()) disabled
-                                                                @endif>
-                                                                Activate
-                                                            </button>
-                                                        </form>
-                                                        <form
-                                                            action="{{ route('teacher.deactivate', ['teacher' => $teacher]) }}"
-                                                            method="post">
-                                                            @csrf
-                                                            @method('PATCH')
-                                                            <button type="submit" class="btn @if (!$teacher->isActive()) btn-primary
-                                                            disabled @else btn-default @endif
-                                                                btn-flat"
-                                                                @if (!$teacher->isActive()) disabled
-                                                                @endif>
-                                                                Deactivate
-                                                            </button>
-                                                        </form>
+                                        @auth('web')
+                                            <div class="tab-pane" id="status">
+                                                <div class="form-group row">
+                                                    {{-- Status --}}
+                                                    <label for="status" class="col-sm-2 col-form-label">Status</label>
+                                                    <div class="col-sm-10">
+                                                        <div class="btn-group">
+                                                            <form
+                                                                action="{{ route('teacher.activate', ['teacher' => $teacher]) }}"
+                                                                method="post">
+                                                                @csrf
+                                                                @method('PATCH')
+                                                                <button type="submit" class="btn @if ($teacher->isActive()) btn-primary
+                                                                disabled @else btn-default @endif
+                                                                    btn-flat"
+                                                                    @if ($teacher->isActive()) disabled
+                                                                    @endif>
+                                                                    Activate
+                                                                </button>
+                                                            </form>
+                                                            <form
+                                                                action="{{ route('teacher.deactivate', ['teacher' => $teacher]) }}"
+                                                                method="post">
+                                                                @csrf
+                                                                @method('PATCH')
+                                                                <button type="submit" class="btn @if (!$teacher->isActive()) btn-primary
+                                                                disabled @else btn-default @endif
+                                                                    btn-flat"
+                                                                    @if (!$teacher->isActive()) disabled
+                                                                    @endif>
+                                                                    Deactivate
+                                                                </button>
+                                                            </form>
 
+                                                        </div>
                                                     </div>
+                                                    {{-- /Status --}}
                                                 </div>
-                                                {{-- /Status --}}
                                             </div>
-                                        </div>
+                                        @endauth
                                         <!-- /.tab-pane -->
-                                        <div class="tab-pane" id="imageUpload">
-                                            <form action="" method="post" id="imageUploadForm"
-                                                enctype="multipart/form-data">
-                                                @csrf
-                                                <div class="form-group">
-                                                    <label for="imageUpload">File input</label>
-                                                    <div class="input-group">
-                                                        <div class="custom-file">
-                                                            <input type="file" name="image" @error('image') is-invalid
-                                                                @enderror class="custom-file-input">
-                                                            <label class="custom-file-label" for="imageUpload">Choose
-                                                                file</label>
+                                        @auth('teacher')
+                                            <div class="tab-pane" id="signature">
+                                                <form
+                                                    action="{{ route('teacher.store.signature', ['teacher' => $teacher]) }}"
+                                                    method="post" id="signatureForm" enctype="multipart/form-data">
+                                                    @method('PATCH')
+                                                    @csrf
+                                                    <div class="form-group">
+                                                        <label for="signature">File input</label>
+                                                        <div class="input-group">
+                                                            <div class="custom-file">
+                                                                <input type="file" name="signature" @error('signature')
+                                                                    is-invalid @enderror class="custom-file-input">
+                                                                <label class="custom-file-label" for="signature">Choose
+                                                                    file</label>
+                                                            </div>
+                                                            <div class="input-group-append">
+                                                                <button type="submit">
+                                                                    <span class="input-group-text">Upload</span></button>
+                                                            </div>
                                                         </div>
-                                                        <div class="input-group-append">
-                                                            <button type="submit">
-                                                                <span class="input-group-text">Upload</span></button>
-                                                        </div>
+                                                        @error('signature')
+                                                            <div class="text-danger">{{ $message }}</div>
+                                                        @enderror
                                                     </div>
-                                                    @error('image')
-                                                        <div class="text-danger">{{ $message }}</div>
-                                                    @enderror
-                                                </div>
-                                            </form>
-                                        </div>
+                                                </form>
+                                            </div>
+                                        @endauth
+
                                     </div>
                                     <!-- /.tab-content -->
                                 </div><!-- /.card-body -->
