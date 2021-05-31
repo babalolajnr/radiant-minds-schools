8b15fb8bd97bd9afca535fd2958f6393e3b845f1
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-05-31 14:24:00 +0100

    fix: remove logo component

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -2,7 +2,6 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                {{-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> --}}
             </a>
         </x-slot>
 
